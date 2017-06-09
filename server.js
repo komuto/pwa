8d48a0d82f3a1f82c1b8ce5814c4824ff7efc0ae
Pesakitan22 <pesakitan22@about.me>
2017-06-09 16:26:54 +0700

    sw precache

--- server.js
+++ server.js
@@ -1,41 +1,30 @@
-const { createServer } = require('http')
-const { parse } = require('url')
+const express = require('express')
 const next = require('next')
-const pathMatch = require('path-match')
+const path = require('path')
 require('dotenv').config()
 
 // front end
 const dev = process.env.NODE_ENV !== 'production'
+const PORT = process.env.PORT || 8889
 const quiet = !dev
 
 const appDir = './'
 const app = next({ dev, dir: appDir, quiet })
-const appHandle = app.getRequestHandler()
+const handle = app.getRequestHandler()
 
-const route = pathMatch()
-const match = route([
-  '/category/:category'
-])
+app.prepare().then(_ => {
+  const server = express()
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
-    const { pathname, query } = parsedUrl
+  // serve service worker
+  server.get('/sw.js', (req, res) =>
+    res.sendFile(path.resolve('./.next/sw.js'))
+  )
 
-    const params = match(pathname)
-    if (params.category) {
-      app.render(req, res, '/category', params)
-      return
-    }
+  server.get('*', (req, res) => handle(req, res))
 
-    if (pathname === '/') {
-      app.render(req, res, '/', query)
-    } else {
-      appHandle(req, res, parsedUrl)
-    }
-  })
-  .listen(3001, (err) => {
+  server.listen(PORT, err => {
     if (err) throw err
+
     console.log(String.raw`
 
       ███████╗██╗  ██╗██╗   ██╗███████╗██╗  ██╗██╗
@@ -46,6 +35,6 @@ app.prepare().then(() => {
       ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝
 
     `)
-    console.log(':fire:> App Steady on http://localhost:3001')
+    console.log(`:fire:> App Steady on http://localhost:${PORT}`)
   })
 })
