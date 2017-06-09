861e8d2e5e6959d2c53c4727af9746731b0a58ca
safei muslim <safeydev@gmail.com>
2017-06-09 13:32:00 +0700

    add jquery, script js

---
+++ static/js/script.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+
+  var $toggle = $('#nav-toggle');
+  var $menu = $('#nav-menu');
+
+  $toggle.click(function() {
+    $(this).toggleClass('is-active');
+    $menu.toggleClass('is-active');
+  });
+
+
+  $('label.checkbox').click(function(){
+    $(this).toggleClass('checked');
+    $(this).find('input').trigger('click');
+    if($(this).hasClass('checked')){
+      $(this).find('input').prop('checked', true);
+    } else {
+      $(this).find('input').prop('checked', false);
+    }
+  });
+  // $('.modal-button').click(function() {
+  //   var target = $(this).data('target');
+  //   $('html').addClass('is-clipped');
+  //   $(target).addClass('is-active');
+  // });
+
+  // $('.modal-background, .modal-close').click(function() {
+  //   $('html').removeClass('is-clipped');
+  //   $(this).parent().removeClass('is-active');
+  // });
+
+  // $('.modal-card-head .delete, .modal-card-foot .button').click(function() {
+  //   $('html').removeClass('is-clipped');
+  //   $('#modal-ter').removeClass('is-active');
+  // });
+
+  // var $highlights = $('.highlight');
+
+  // $highlights.each(function() {
+  //   var $el = $(this);
+  //   var copy = '<button class="copy">Copy</button>';
+  //   var expand = '<button class="expand">Expand</button>';
+  //   $el.append(copy);
+
+  //   if ($el.find('pre code').innerHeight() > 600) {
+  //     $el.append(expand);
+  //   }
+  // });
+
+  // var $highlightButtons = $('.highlight .copy, .highlight .expand');
+
+  // $highlightButtons.hover(function() {
+  //   $(this).parent().css('box-shadow', '0 0 0 1px #ed6c63');
+  // }, function() {
+  //   $(this).parent().css('box-shadow', 'none');
+  // });
+
+  // $('.highlight .expand').click(function() {
+  //   $(this).parent().children('pre').css('max-height', 'none');
+  // });
+
+  // new Clipboard('.copy', {
+  //   target: function(trigger) {
+  //     return trigger.previousSibling;
+  //   }
+  // });
+
+});
