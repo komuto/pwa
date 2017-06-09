083b6531d4f9b1413d456bc461eacad83a6385dc
safei muslim <safeydev@gmail.com>
2017-06-09 13:29:23 +0700

    define image

--- src/Themes/Images.js
+++ src/Themes/Images.js
@@ -1,5 +1,8 @@
 const images = {
-  about: 'https://skyshi.com/sites/skyshi.com/files/logo_rev_1.png'
+  about: 'https://skyshi.com/sites/skyshi.com/files/logo_rev_1.png',
+  password: '../../static/images/password.svg',
+  phoneAccount: '../../static/images/phone-account.svg',
+  regSuccess: '../../static/images/reg-success.svg'
 }
 
 export default images
