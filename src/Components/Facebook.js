d1be8f04194410914c9372d1bf99ec7881e4f6ca
safei muslim <safeydev@gmail.com>
2017-06-13 23:11:35 +0700

    add Facebook login component

---
+++ src/Components/Facebook.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import FacebookLogin from 'react-facebook-login'
+
+export const LoginFacebook = (props) => {
+  return <FacebookLogin
+    appId='829312977224065'
+    autoLoad
+    fields='name,email,picture'
+    textButton='Login dengan facebook'
+    callback={(response) => props.responseFacebook(response)}
+    cssClass='button is-fb is-large is-fullwidth'
+    icon={<span className='icon-fb' />} />
+}
