bfa4a755ec055b9f5f2f5c441febf720c4d9b7d7
safei muslim <safeydev@gmail.com>
2017-06-13 23:12:50 +0700

    traslate to react component

--- src/Containers/SignIn.js
+++ src/Containers/SignIn.js
@@ -1,45 +1,121 @@
 // @flow
 import React, { Component } from 'react'
+import Link from 'next/link'
+import * as EmailValidator from 'email-validator'
+// components
+import { LoginFacebook } from '../Components/Facebook'
+import { Navigation, Hero } from '../Components/Navigation'
+import { Input } from '../Components/Input'
+import { ButtonFullWidth } from '../Components/Button'
+import { HrText } from '../Components/Hr'
+import TermConditions from '../Components/TermConditions'
+
 class SignIn extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      input: {
+        email: {
+          type: 'email',
+          placeholder: 'Email',
+          name: 'email',
+          value: '',
+          classInfo: '',
+          textHelp: ''
+        },
+        password: {
+          type: 'password',
+          placeholder: 'Password',
+          name: 'password',
+          value: '',
+          classInfo: '',
+          textHelp: ''
+        }
+      }
+    }
+    this.onChange = this.onChange.bind(this)
+  }
+
+  responseFacebook (response) {
+    console.log(response)
+  }
+
+  handleSignInClick () {
+    console.log('handleSignInClick ()')
+  }
+
+  onChange (event) {
+    const name = event.target.name
+    const targetValue = event.target.value
+    let { input } = this.state
+    switch (name) {
+      case input.email.name:
+        input.email.value = targetValue
+        if (EmailValidator.validate(targetValue)) {
+          input.email.classInfo = 'is-success'
+          input.email.textHelp = 'Format email sudah benar'
+        } else if (targetValue === '') {
+          input.email.classInfo = ''
+          input.email.textHelp = ''
+        } else {
+          input.email.classInfo = 'is-danger'
+          input.email.textHelp = 'Format email salah'
+        }
+        break
+      case input.password.name:
+        input.password.value = targetValue
+        input.password.classInfo = 'is-success'
+        break
+      default:
+        break
+    }
+    this.setState({ input })
+  }
+
   render () {
+    const { input } = this.state
     return (
       <div className='main user login'>
-        <nav className='level header is-fullwidth'>
-          <div className='nav-left'>
-            <a href='profile' className='level-item'>
-              <span><span className='icon-arrow-left' /> Login</span>
-            </a>
-          </div>
-        </nav>
-        <section className='hero'>
-          <div className='container is-fluid'>
-            <p>Belum punya akun? <a href='signup'>Daftar Disini</a></p>
-          </div>
-        </section>
+        <Navigation
+          path='/profile'
+          icon={<span className='icon-arrow-left' />}
+          textPath='Login' />
+        <Hero
+          path='/signup'
+          textPath='Daftar Disini'
+          textInfo='Belum punya akun?' />
+
         <section className='content'>
           <div className='container is-fluid'>
             <form action='#' className='form'>
-              <div className='field'>
-                <p className='control'>
-                  <input className='input' type='email' placeholder='Alamat Email' />
-                </p>
+              <Input
+                type={input.email.type}
+                placeholder={input.email.placeholder}
+                name={input.email.name}
+                classInfo={input.email.classInfo}
+                value={input.email.value}
+                onChange={this.onChange}
+                hasIconsRight
+                textHelp={input.email.textHelp} />
+              <Input
+                type={input.password.type}
+                placeholder={input.password.placeholder}
+                name={input.password.name}
+                classInfo={input.password.classInfo}
+                value={input.password.value}
+                onChange={this.onChange}
+                hasIconsRight
+                textHelp={input.password.textHelp} />
+              <TermConditions />
+              <ButtonFullWidth
+                text='Login'
+                onClick={this.handleSignInClick} />
+              <div className='has-text-centered'>
+                <Link href='/password-reset'><a>Lupa Password</a></Link>
               </div>
-              <div className='field'>
-                <p className='control'>
-                  <input className='input' type='password' placeholder='Password' />
-                </p>
-              </div>
-              <div className='field'>
-                <p>Dengan mendaftar Anda telah menyetujui <a href='#'>Syarat dan Ketentuan</a> dari Komuto</p>
-              </div>
-
-              <a className='button is-primary is-large is-fullwidth'>Login</a>
-
-              <div className='has-text-centered'><a href='password-reset'>Lupa Password</a></div>
-              <div className='or'>
-                <span>Atau</span>
-              </div>
-              <a className='button is-fb is-large is-fullwidth'>Login dengan facebook</a>
+              <HrText text='atau' />
+              <LoginFacebook
+                responseFacebook={this.responseFacebook} />
             </form>
           </div>
         </section>
