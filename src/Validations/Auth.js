b901ba6d55992c053d4c88dde35ce577b8c5b0a8
safei muslim <safeydev@gmail.com>
2017-06-14 10:19:00 +0700

    add validations constrains auth

--- src/Validations/Auth.js
+++ src/Validations/Auth.js
@@ -1,16 +1,44 @@
-const loginConstraints = {
+export const classInfo = {
+  danger: 'is-danger',
+  success: 'is-success'
+}
+export const loginConstraints = {
+  nama: {
+    alert: {
+      empty: 'Nama tidak boleh kosong',
+      valid: 'Nama anda tidak valid'
+    }
+  },
+  handphone: {
+    regex: /^(^\+62\s?|^0)(\d{3,4}-?){2}\d{3,4}$/g,
+    alert: {
+      empty: 'No handphone tidak boleh kosong',
+      valid: 'No handphone anda tidak valid'
+    }
+  },
   email: {
     email: true,
-    presence: true
+    presence: true,
+    alert: {
+      empty: 'Email tidak boleh kosong',
+      valid: 'Format email anda tidak tepat'
+    }
   },
   password: {
     presence: true,
+    alert: {
+      empty: 'Password tidak boleh kosong',
+      valid: 'Password tidak kuat'
+    },
     length: {
       minimum: 5
     }
+  },
+  retypePassword: {
+    presence: true,
+    alert: {
+      empty: 'Konfirmasi Password tidak boleh kosong',
+      valid: 'Konfirmasi password salah'
+    }
   }
-}
-
-export default {
-  loginConstraints
 }
