fbdb59c8cea948ec0e4d9d4b7e24432dbeb82a60
safei muslim <safeydev@gmail.com>
2017-06-14 16:22:28 +0700

    update redux

--- src/Utils/ReduxPageWrapper.js
+++ src/Utils/ReduxPageWrapper.js
@@ -1,25 +1,18 @@
 // @flow
 import React, {Component} from 'react'
-import initStore from '../Redux'
-import rootSaga from '../Sagas'
-import { sagaMiddleware } from '../Redux/CreateStore'
-import BannerActions from '../Redux/BannerRedux'
+import dataSaga from '../saga/saga'
+import { sagaMiddleware, store } from '../store'
 import withRedux from 'next-redux-wrapper'
-import {END} from 'redux-saga'
+// import {END} from 'redux-saga'
 
 let clientTask = null
 
 export default function reduxWrapper (ReduxComponent:Object) {
   class ReduxContainer extends Component {
-    static async getInitialProps ({ store, req, isServer, query }) {
+    static async getInitialProps ({ req, isServer, query }) {
       if (isServer) {
-        const rootTask = sagaMiddleware.run(rootSaga)
-        // dispatch server sagas here
-        store.dispatch(BannerActions.bannerServer())
-        store.dispatch(END)
-        await rootTask.done.then(() => {
-
-        })
+        // /const rootTask = sagaMiddleware.run(dataSaga)
+        sagaMiddleware.run(dataSaga)
         return {query}
       } else {
         return {query}
@@ -29,7 +22,7 @@ export default function reduxWrapper (ReduxComponent:Object) {
     constructor (props:any) {
       super(props)
       if (!clientTask) {
-        clientTask = sagaMiddleware.run(rootSaga)
+        clientTask = sagaMiddleware.run(dataSaga)
       }
     }
 
@@ -39,5 +32,5 @@ export default function reduxWrapper (ReduxComponent:Object) {
       )
     }
   }
-  return withRedux(initStore)(ReduxContainer)
+  return withRedux(store)(ReduxContainer)
 }
