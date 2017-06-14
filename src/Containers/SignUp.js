ad94f7521f07c25ad603206a2a34499a4b3a6b43
safei muslim <safeydev@gmail.com>
2017-06-14 10:18:40 +0700

    add validations

--- src/Containers/SignUp.js
+++ src/Containers/SignUp.js
@@ -8,6 +8,8 @@ import { Input, InputRadio } from '../Components/Input'
 import { ButtonFullWidth } from '../Components/Button'
 import { HrText } from '../Components/Hr'
 import TermConditions from '../Components/TermConditions'
+// validations
+import * as constraints from '../Validations/Auth'
 
 class SignUp extends Component {
   constructor (props) {
@@ -76,47 +78,79 @@ class SignUp extends Component {
   }
 
   onChange (event) {
-    const name = event.target.name
-    const targetValue = event.target.value
+    const { nama, handphone, email, password, retypePassword } = constraints.loginConstraints
+    const { danger, success } = constraints.classInfo
+    const { name, value } = event.target
     let { input } = this.state
 
     switch (name) {
       case input.nama.name:
-        input.nama.value = targetValue
-        input.nama.classInfo = 'is-success'
+        input.nama.value = value
+        if (value === '') {
+          input.nama.classInfo = danger
+          input.nama.textHelp = nama.alert.empty
+        } else {
+          input.nama.classInfo = success
+        }
         break
       case input.handphone.name:
-        input.handphone.value = targetValue
-        input.handphone.classInfo = 'is-success'
+        input.handphone.value = value
+        if (value === '') {
+          input.handphone.classInfo = danger
+          input.handphone.textHelp = handphone.alert.empty
+        } else {
+          if (value.match(handphone.regex)) {
+            input.handphone.classInfo = success
+            input.handphone.textHelp = ''
+          } else {
+            input.handphone.classInfo = danger
+            input.handphone.textHelp = handphone.alert.valid
+          }
+        }
         break
       case input.email.name:
-        input.email.value = targetValue
-        if (EmailValidator.validate(targetValue)) {
-          input.email.classInfo = 'is-success'
-          input.email.textHelp = 'Email anda sudah benar'
+        input.email.value = value
+        if (value === '') {
+          input.email.classInfo = danger
+          input.email.textHelp = email.alert.empty
         } else {
-          input.email.classInfo = 'is-danger'
-          input.email.textHelp = 'Silahkan input email dengan benar'
+          if (EmailValidator.validate(value)) {
+            input.email.classInfo = success
+            input.email.textHelp = ''
+          } else {
+            input.email.classInfo = danger
+            input.email.textHelp = email.alert.valid
+          }
         }
         break
       case input.password.name:
-        input.password.value = targetValue
-        if ([...targetValue].length < 6) {
-          input.password.classInfo = 'is-danger'
-          input.password.textHelp = 'Password tidak aman'
+        input.password.value = value
+        if (value === '') {
+          input.password.classInfo = danger
+          input.password.textHelp = password.alert.empty
         } else {
-          input.password.classInfo = 'is-success'
-          input.password.textHelp = 'Password sudah aman'
+          if ([...value].length < password.length.minimum) {
+            input.password.classInfo = danger
+            input.password.textHelp = password.alert.valid
+          } else {
+            input.password.classInfo = success
+            input.password.textHelp = ''
+          }
         }
         break
       case input.passwordRetype.name:
-        input.passwordRetype.value = targetValue
-        if (input.password.value !== targetValue) {
-          input.passwordRetype.classInfo = 'is-danger'
-          input.passwordRetype.textHelp = 'Konfirmasi password tidak sama'
+        input.passwordRetype.value = value
+        if (value === '') {
+          input.passwordRetype.classInfo = danger
+          input.passwordRetype.textHelp = password.alert.empty
         } else {
-          input.passwordRetype.classInfo = 'is-success'
-          input.passwordRetype.textHelp = 'Konfirmasi password sudah sama'
+          if (input.password.value !== value) {
+            input.passwordRetype.classInfo = danger
+            input.passwordRetype.textHelp = retypePassword.alert.valid
+          } else {
+            input.passwordRetype.classInfo = success
+            input.passwordRetype.textHelp = ''
+          }
         }
         break
       default:
