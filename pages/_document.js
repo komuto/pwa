dcf7678e77cbfa5d035599961bd5dfb371b78dd4
safei muslim <safeydev@gmail.com>
2017-06-14 16:21:27 +0700

    update progress

--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,7 @@ export default class HomeDocument extends Document {
           <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport' />
           <meta name='apple-mobile-web-app-status-bar-style' content='black' />
           <link rel='stylesheet prefetch' href={`static/dist/css/style.css`} />
+          <link rel='stylesheet prefetch' href={`static/dist/css/nprogress.css`} />
           <meta name='theme-color' content='#ff6600' />
           <link rel='manifest' href='static/manifest.json' />
         </Head>
@@ -26,6 +27,7 @@ export default class HomeDocument extends Document {
           <script src={`static/js/index.js`} />
           <NextScript />
           <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js' />
+          <script src='static/js/nprogress.js' />
         </body>
       </html>
     )
