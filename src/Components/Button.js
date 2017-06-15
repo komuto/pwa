ddd7cc619bd6f8703685e95cc9f313ac3cfe3f6c
safei muslim <safeydev@gmail.com>
2017-06-15 14:26:36 +0700

    ‘ADD BUTTON COMPONENT’

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,3 +1,12 @@
+// @flow
+import React from 'react'
+import Link from 'next/link'
+
+export const ButtonFullWidthLink = (props) => {
+  return (
+    <Link href={props.path}><a className={`button is-primary is-large is-fullwidth ${props.className}`}>{props.text}</a></Link>
+  )
+}
 
 export const ButtonFullWidth = (props) => {
   return (
