58325339a7cb397f18fa75db74e96702770c29ce
safei muslim <safeydev@gmail.com>
2017-06-17 07:59:16 +0700

    add new asset

--- src/Themes/Images.js
+++ src/Themes/Images.js
@@ -2,7 +2,8 @@ const images = {
   about: 'https://skyshi.com/sites/skyshi.com/files/logo_rev_1.png',
   password: '../../static/images/password.svg',
   phoneAccount: '../../static/images/phone-account.svg',
-  regSuccess: '../../static/images/reg-success.svg'
+  regSuccess: '../../static/images/reg-success.svg',
+  user: '../../static/images/pict.jpg'
 }
 
 export default images
