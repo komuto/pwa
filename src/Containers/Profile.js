f0c8266cc241df57541a8edfb2761a95c2840f4e
safei muslim <safeydev@gmail.com>
2017-06-19 11:10:54 +0700

    update data profile

--- src/Containers/Profile.js
+++ src/Containers/Profile.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react'
 // import Link from 'next/link'
 // import {Images} from '../Themes'
-// import localForage from 'localforage'
+import localForage from 'localforage'
 // components
 import { Navigation } from '../Components/Navigation'
 import Tabbar, { PROFILE } from '../Components/Tabbar'
@@ -12,33 +12,27 @@ class Profile extends Component {
   constructor (props) {
     super(props)
     this.state = ({
-      user: null
+      user: null,
+      sessionReady: false
     })
   }
-  componentWillMount () {
-    // localForage.getItem('sessionLogin').then((value) => {
-    //   this.setState({ user: value })
-    // })
-  }
 
-  componentWillReceiveProps (nextProps) {
-    // const { pathname, query } = nextProps.url
-    // if (query.user) {
-    //   this.setState({
-    //     user: query.user
-    //   })
-    // }
+  componentWillMount () {
+    localForage.getItem('sessionLogin').then((value) => {
+      this.setState({ user: value, sessionReady: true })
+    })
   }
 
   render () {
-    const { user } = this.state
+    const { sessionReady, user } = this.state
+    if (!sessionReady) return null
     return (
       <div className='main user bg-grey'>
         <Navigation
           path=''
           icon=''
           textPath='Profile' />
-        { (user) ? <Account /> : <AccountLogin /> }
+        { (user) ? <Account user={user} /> : <AccountLogin /> }
         <Tabbar active={PROFILE} />
       </div>
     )
