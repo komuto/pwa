fe04bdcddfe72237a53bbfa3c6c88114d7037a73
safei muslim <safeydev@gmail.com>
2017-06-19 13:11:18 +0700

    add email users

--- src/Containers/SignUpVerification.js
+++ src/Containers/SignUpVerification.js
@@ -1,13 +1,15 @@
 // @flow
 import React from 'react'
-import {Images} from '../Themes'
+import Router from 'next/router'
+import { Images } from '../Themes'
 import { NotificationPage } from '../Components/Notification'
 
 export default (props:any) => {
+  const email = Router.query.email
   return <NotificationPage
     icon={Images.password}
     title='Link Verifikasi Email telah terkirim'
-    subTitle='Kami telah mengirim link verifikasi email ke dwinawan@gmail.com. Silahkan periksa inbox Anda, dan ikuti petunjuk di email tersebut.'
+    subTitle={`Kami telah mengirim link verifikasi email ke ${email}. Silahkan periksa inbox Anda, dan ikuti petunjuk di email tersebut.`}
     path='signin'
     text='Ke Halaman Login' />
 }
