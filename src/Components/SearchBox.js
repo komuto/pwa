f90da9ddd7687d3e88ed0eba4dac1914ae60fe7c
safei muslim <safeydev@gmail.com>
2017-06-21 11:49:28 +0700

    Add searchBoox Component

---
+++ src/Components/SearchBox.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+class SearchBox extends Component {
+  render () {
+    return (
+      <nav className='level header is-fullwidth'>
+        <div className='nav-left'>
+          <a className='level-item'>
+            <span>Galaksi Parabola</span>
+          </a>
+        </div>
+        <div className='nav-right'>
+          <span className='icon-love' />
+          <span className='icon-cart'><span className='notif-cart'>4</span></span>
+        </div>
+        <div className='field search-form is-clearfix'>
+          <p className='control has-icons-left'>
+            <input className='input is-medium' type='text' placeholder='Cari barang atau toko' />
+            <span className='icon is-left'>
+              <span className='icon-search' />
+            </span>
+          </p>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default SearchBox
