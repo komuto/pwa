c732d1e07f4693cae05b8b29f28c4fc3764efce9
safei muslim <safeydev@gmail.com>
2017-06-21 11:51:24 +0700

    Update dynamic email

--- src/Containers/PasswordResetVerification.js
+++ src/Containers/PasswordResetVerification.js
@@ -1,13 +1,15 @@
 // @flow
 import React from 'react'
+import Router from 'next/router'
 import {Images} from '../Themes'
 import { NotificationPage } from '../Components/Notification'
 
 export default (props:any) => {
+  const email = Router.query.email
   return <NotificationPage
     icon={Images.regSuccess}
     title='Link Reset Password telah terkirim'
-    subTitle='Kami telah mengirim link reset password ke dwinawan@gmail.com. Silahkan periksa inbox Anda, dan ikuti petunjuk di email tersebut.'
+    subTitle={`Kami telah mengirim link reset password ke ${email}. Silahkan periksa inbox Anda, dan ikuti petunjuk di email tersebut.`}
     path='signin'
     text='Ke Halaman Login' />
 }
