78528c16c84e6b723bc245147c4d7acac8dc3623
safei muslim <safeydev@gmail.com>
2017-06-21 11:47:01 +0700

    add home component

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,31 @@
 // @flow
 // layout
+import DefaultLayout from '../src/Layout/DefaultLayout'
 // wrapper
 import ReduxPageWrapper from '../src/Utils/ReduxPageWrapper'
-// containers
-import Profile from '../src/Containers/Profile'
-// style custom
+// container
+import { HOME } from '../src/Containers/Tabbar'
+import Home from '../src/Containers/Home'
+
+const params = {
+  style: 'home bg-grey',
+  header: {
+    title: 'Komuto'
+  },
+  navbar: {
+    searchBoox: true,
+    textPath: 'Galaksi Parabola'
+  },
+  tabbar: {
+    active: HOME,
+    isShow: true
+  }
+}
 
 const Index = () => (
-  <Profile />
+  <DefaultLayout params={params}>
+    <Home />
+  </DefaultLayout>
 )
 
 export default ReduxPageWrapper(Index)
