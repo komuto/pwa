f5aaa12ca31a18d2b580affbbedaba7eea1ff9aa
safei muslim <safeydev@gmail.com>
2017-06-22 16:26:15 +0700

    add product new page

---
+++ pages/product-new.js
@@ -0,0 +1,27 @@
+// @flow
+// layout
+import DefaultLayout from '../src/Layout/DefaultLayout'
+// wrapper
+import ReduxPageWrapper from '../src/Utils/ReduxPageWrapper'
+// containers
+import Categories4 from '../src/Containers/Categories4'
+
+const params = {
+  style: 'categories bg-grey',
+  header: {
+    title: 'Produk Terbaru'
+  },
+  navbar: {
+    searchBoox: false,
+    path: '/',
+    textPath: 'Produk Terbaru'
+  }
+}
+
+const Index = () => (
+  <DefaultLayout params={params}>
+    <Categories4 />
+  </DefaultLayout>
+)
+
+export default ReduxPageWrapper(Index)
