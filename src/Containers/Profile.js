a5ae80c6ed8d142c895b63be4b50f2ed46b9fde2
safei muslim <safeydev@gmail.com>
2017-06-23 15:39:45 +0700

    update profile

--- src/Containers/Profile.js
+++ src/Containers/Profile.js
@@ -1,31 +1,79 @@
 // @flow
 import React, { Component } from 'react'
-// import Link from 'next/link'
-// import {Images} from '../Themes'
-import localForage from 'localforage'
+import { connect } from 'react-redux'
+import NProgress from 'nprogress'
 // components
+import Content from '../Components/Content'
 import AccountLogin from './AccountLogin'
 import Account from './Account'
+import Notification from '../Components/Notification'
+// actions
+import * as loginAction from '../actions/user'
+
 class Profile extends Component {
   constructor (props) {
     super(props)
     this.state = ({
-      user: null,
-      sessionReady: false
+      user: props.profile.user || null,
+      notification: {
+        status: false,
+        message: 'Error, default message.'
+      }
     })
   }
 
-  componentWillMount () {
-    localForage.getItem('sessionLogin').then((value) => {
-      this.setState({ user: value, sessionReady: true })
-    })
+  async componentDidMount () {
+    if (this.state.user) {
+      NProgress.start()
+      await this.props.dispatch(loginAction.getProfile())
+    }
+  }
+
+  componentWillReceiveProps (nextProps) {
+    const { profile } = nextProps
+    const { notification } = this.state
+
+    if (!profile.isOnline) {
+      NProgress.done()
+      notification.status = true
+      notification.message = profile.message
+    }
+
+    //  if (!profile.isFound) {
+    //   NProgress.done()
+    //   notification.status = true
+    //   notification.message = 'Data tidak di temukan'
+    //  }
+
+    if (profile.isFound && !profile.isLoading) {
+      NProgress.done()
+      notification.status = false
+      notification.message = ''
+      this.setState({ user: profile.user })
+    }
   }
 
   render () {
-    const { sessionReady, user } = this.state
-    if (!sessionReady) return null
-    return (user) ? <Account user={user} /> : <AccountLogin />
+    let { user, notification } = this.state
+    user = null
+    return (
+      <Content>
+        <Notification
+          type='is-warning'
+          isShow={notification.status}
+          activeClose
+          onClose={() => this.setState({notification: {status: false, message: ''}})}
+          message={notification.message} />
+        { (user) ? <Account user={user} notification={notification} /> : <AccountLogin /> }
+      </Content>
+    )
   }
 }
 
-export default Profile
+const mapStateToProps = (state) => {
+  return {
+    profile: state.profile
+  }
+}
+
+export default connect(mapStateToProps)(Profile)
