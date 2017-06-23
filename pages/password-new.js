c4ccff6fdc977f21d055c2044a0ff14fc6638dff
safei muslim <safeydev@gmail.com>
2017-06-23 15:38:42 +0700

    add password new component

--- pages/password-new.js
+++ pages/password-new.js
@@ -1,13 +1,26 @@
 // @flow
 // layout
-// import DefaultLayout from '../src/Layout/DefaultLayout'
+import DefaultLayout from '../src/Layout/DefaultLayout'
 // wrapper
 import ReduxPageWrapper from '../src/Utils/ReduxPageWrapper'
 // containers
 import PasswordNew from '../src/Containers/PasswordNew'
 // style custom
-const Index = () => (
-  <PasswordNew />
-)
+const params = {
+  style: 'user login',
+  header: {
+    title: 'Password Baru'
+  },
+  navbar: {
+    searchBoox: false,
+    path: '/',
+    textPath: 'Password Baru'
+  }
+}
 
+const Index = () => (
+  <DefaultLayout params={params}>
+    <PasswordNew />
+  </DefaultLayout>
+)
 export default ReduxPageWrapper(Index)
