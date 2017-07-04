ac45adcdc4b43fc1aa029164927924b7b88dc0e3
safei muslim <safeydev@gmail.com>
2017-07-04 10:28:16 +0700

    - add loads - optimize respon

--- src/Containers/Profile.js
+++ src/Containers/Profile.js
@@ -2,13 +2,18 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import NProgress from 'nprogress'
+import _ from 'lodash'
 // components
 import Content from '../Components/Content'
 import AccountLogin from './AccountLogin'
 import Account from './Account'
 import Notification from '../Components/Notification'
+// services
+import GET_TOKEN from '../Services/GetToken'
 // actions
 import * as loginAction from '../actions/user'
+// utils
+import { Status } from '../Services/Status'
 
 class Profile extends Component {
   constructor (props) {
@@ -23,54 +28,57 @@ class Profile extends Component {
   }
 
   async componentDidMount () {
-    if (this.state.user) {
+    const { user } = this.state
+    const token = await GET_TOKEN.getToken()
+
+    if (token && _.isEmpty(user)) {
       NProgress.start()
-      await this.props.dispatch(loginAction.getProfile())
+      this.props.dispatch(loginAction.getProfile())
     }
   }
 
   componentWillReceiveProps (nextProps) {
     const { profile } = nextProps
-    const { notification } = this.state
 
-    if (!profile.isOnline) {
+    if (!profile.isLoading) {
       NProgress.done()
-      notification.status = true
-      notification.message = profile.message
-    }
-
-    //  if (!profile.isFound) {
-    //   NProgress.done()
-    //   notification.status = true
-    //   notification.message = 'Data tidak di temukan'
-    //  }
-
-    if (profile.isFound && !profile.isLoading) {
-      NProgress.done()
-      notification.status = false
-      notification.message = ''
-      this.setState({ user: profile.user })
+      switch (profile.status) {
+        case Status.SUCCESS :
+          (profile.isFound)
+          ? this.setState({ user: profile.user })
+          : this.setState({ notification: {status: true, message: 'Data tidak ditemukan'} })
+          break
+        case Status.OFFLINE :
+        case Status.FAILED :
+          this.setState({ notification: {status: true, message: profile.message} })
+          break
+        default:
+          break
+      }
     }
   }
 
   render () {
     let { user, notification } = this.state
-    user = null
     return (
       <Content>
         <Notification
-          type='is-warning'
+          type='is-danger'
           isShow={notification.status}
           activeClose
           onClose={() => this.setState({notification: {status: false, message: ''}})}
           message={notification.message} />
-        { (user) ? <Account user={user} notification={notification} /> : <AccountLogin /> }
+        { (user)
+          ? <Account user={user} />
+          : <AccountLogin />
+        }
       </Content>
     )
   }
 }
 
 const mapStateToProps = (state) => {
+  console.log(state.profile)
   return {
     profile: state.profile
   }
