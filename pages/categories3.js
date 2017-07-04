db96f19f80575b4a048f50c0f247c97eb0528e8b
safei muslim <safeydev@gmail.com>
2017-07-04 17:46:51 +0700

    add DynamicNavBarLayout

--- pages/categories3.js
+++ pages/categories3.js
@@ -1,6 +1,6 @@
 // @flow
 // layout
-import DefaultLayout from '../src/Layout/DefaultLayout'
+import DynamicNavBarLayout from '../src/Layout/DefaultLayout'
 // wrapper
 import ReduxPageWrapper from '../src/Utils/ReduxPageWrapper'
 // containers
@@ -9,19 +9,16 @@ import Categories3 from '../src/Containers/Categories3'
 const params = {
   style: 'categories bg-grey',
   header: {
-    title: 'Sepatu Pria'
-  },
-  navbar: {
-    searchBoox: false,
-    path: 'categories2',
-    textPath: 'Sepatu Pria'
+    title: 'Komuto'
   }
 }
 
-const Index = () => (
-  <DefaultLayout params={params}>
-    <Categories3 />
-  </DefaultLayout>
-)
+const Index = (props) => {
+  return (
+    <DynamicNavBarLayout params={params}>
+      <Categories3 query={props.query} />
+    </DynamicNavBarLayout>
+  )
+}
 
 export default ReduxPageWrapper(Index)
