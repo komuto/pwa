c13980dd8847e4705ba96f83842d8d625fec1665
safei muslim <safeydev@gmail.com>
2017-07-11 16:28:14 +0700

    - change from const to react class - add react-image

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,42 +1,57 @@
+import React, { Component } from 'react'
+import Img from 'react-image'
+// themes
+import Images from '../Themes/Images'
 // lib
 import RupiahFormat from '../Lib/RupiahFormat'
 // pin type: gross is grossir, disc is discount
 
-export default (props:any) => {
-  const {product, images, store, viewActive} = props
-  let pin = null
-  if (product.is_discount) pin = <div className='pin disc'><span>{ `${product.discount}%` }</span></div>
-  // if (product.is_dropshipper) pin = <div className='pin'><span>Dropship</span></div>
-  if (product.is_wholesaler) pin = <div className='pin'><span>Grossir</span></div>
+class Product extends Component {
+  render () {
+    const { product, images, store, viewActive } = this.props
+    const loader = <img src={Images.loading} />
+    const unloader = <img src={Images.loadingFailed} />
+    // set pin
+    let pin = null
+    if (product.is_discount) pin = <div className='pin disc'><span>{ `${product.discount}%` }</span></div>
+    if (product.is_wholesaler) pin = <div className='pin'><span>Grossir</span></div>
+    const thumb = images[0].file
+    // const thumb = 'https://komutodev.aptmi.com/uploads/produk/8011774ba21b8cc99a20583008bc07e43d19bdc1_klepon5.jpg'
+    // set real price
+    const priceBeforeDiscount = (product.discount > 0) ? <div className='discount'> Rp { RupiahFormat(product.price) } </div> : ''
+    // set price - dicsount
+    const priceAfterDiscount = (product.discount > 0) ? (product.price - (product.price * (product.discount / 100))) : product.price
 
-  // const thumb = imagesDefault
-  let thumb = images[0].file
-  const priceBeforeDiscount = (product.discount > 0) ? <div className='discount'> Rp { RupiahFormat(product.price) } </div> : ''
-  const priceAfterDiscount = (product.discount > 0) ? (product.price - (product.price * (product.discount / 100))) : product.price
-
-  return (
-    <div className={`column ${viewActive === 'grid' ? 'is-half' : ''}`}>
-      <div className={`box ${viewActive}`} >
-        <div className='media'>
-          <div className='media-left'>
-            <figure className='image'>
-              <a><img src={thumb} alt='Image' style={{ width: '150px' }} /></a>
-              { pin }
-            </figure>
-          </div>
-          <div className='media-content'>
-            <div className='content'>
-              <h4>{product.name}</h4>
-              <div className='detail'>
-                <p>{store.name} { (store.is_verified) ? <span className='icon-verified' /> : '' }</p>
-                { priceBeforeDiscount }
-                <span className='price' style={{ width: '100%' }}>Rp { RupiahFormat(priceAfterDiscount) } </span>
-                <span className='wish'><span className='icon-wishlist wishlisted' /> { product.count_like } </span>
+    return (
+      <div className={`column ${viewActive === 'grid' ? 'is-half' : ''}`}>
+        <div className={`box ${viewActive}`} >
+          <div className='media'>
+            <div className='media-left'>
+              <figure className='image' style={{width: '150px'}}>
+                <Img
+                  src={thumb}
+                  loader={loader}
+                  unloader={unloader}
+                  />
+                { pin }
+              </figure>
+            </div>
+            <div className='media-content'>
+              <div className='content'>
+                <h4>{product.name}</h4>
+                <div className='detail'>
+                  <p>{store.name} { (store.is_verified) ? <span className='icon-verified' /> : '' }</p>
+                  { priceBeforeDiscount }
+                  <span className='price' style={{ width: '100%' }}>Rp { RupiahFormat(priceAfterDiscount) } </span>
+                  <span className='wish'><span className='icon-wishlist wishlisted' /> { product.count_like } </span>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
+
+export default Product
