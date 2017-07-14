afef77656880eadb002c199fc94bb256acdb5e6a
safei muslim <safeydev@gmail.com>
2017-07-14 11:34:39 +0700

    add status filter off

--- src/Containers/TabbarCategories.js
+++ src/Containers/TabbarCategories.js
@@ -2,15 +2,20 @@
 import React from 'react'
 
 const Tabbar = (props) => {
-  const { viewActive } = props
+  const { viewActive, sortButton, filterButton, viewButton } = props
+  const sort = sortButton ? <a onClick={props.sortOnClick} className='level-item has-text-centered js-sort'><span className='icon-sort' />Urutkan</a> : null
+  const filter = filterButton ? <a onClick={props.filterOnClick} className='level-item has-text-centered modal-button' data-target='#modal-filter'><span className='icon-filter' />Filter</a> : null
+  const view = viewButton
+                ? <a onClick={props.viewOnClick} className='level-item has-text-centered'>
+                  { (viewActive === 'list') ? <span className='icon-grid-view' /> : <span className='icon-list' /> }
+                    Tampilan
+                  </a>
+                : null
   return (
     <div className='level nav-bottom filter-nav is-mobile'>
-      <a onClick={props.sortOnClick} className='level-item has-text-centered js-sort'><span className='icon-sort' />Urutkan</a>
-      <a onClick={props.filterOnClick} className='level-item has-text-centered modal-button' data-target='#modal-filter'><span className='icon-filter' />Filter</a>
-      <a onClick={props.viewOnClick} className='level-item has-text-centered'>
-        { (viewActive === 'list') ? <span className='icon-grid-view' /> : <span className='icon-list' /> }
-        Tampilan
-      </a>
+      { sort }
+      { filter }
+      { view }
     </div>
   )
 }
