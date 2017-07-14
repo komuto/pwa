39573c266a06ea1e8ebf0deb59534a88783de25f
safei muslim <safeydev@gmail.com>
2017-07-14 11:36:22 +0700

    - add image review start  - add image loading failed

--- src/Themes/Images.js
+++ src/Themes/Images.js
@@ -8,7 +8,9 @@ const images = {
   thumb: '../../static/images/thumb.jpg',
   noImage: '../../static/images/imagenotfound.svg',
   loading: '../../static/images/loading.png',
-  loadingFailed: '../../static/images/loading-failed.png'
+  loadingFailed: '../../static/images/loading-failed.png',
+  startDisabled: '../../static/images/komuto-icon/050_star-grey.svg',
+  startEnabled: '../../static/images/komuto-icon/051_star_yellow.svg'
 }
 
 export default images
