e2cb8da6e2c80353361b7e0243e18214bb2b551a
safei muslim <safeydev@gmail.com>
2017-07-20 09:23:33 +0700

    ‘KOM-422 Estimasi Pengiriman’

--- src/Containers/ProductDetailServices.js
+++ src/Containers/ProductDetailServices.js
@@ -4,12 +4,18 @@ import { connect } from 'react-redux'
 import Section from '../Components/Section'
 // actions
 import * as locationActions from '../actions/location'
+import * as expeditionActions from '../actions/expedition'
+// services
+import { Status } from '../Services/Status'
 
 class ProductDetailServices extends Component {
   constructor (props) {
     super(props)
     this.state = {
+      estimatedCharges: props.estimatedCharges || null,
       countProduct: 0,
+      totWeight: 0,
+      formActive: false,
       provinces: {
         data: props.provinces || null,
         show: false,
@@ -70,8 +76,18 @@ class ProductDetailServices extends Component {
     })
   }
 
-  async subDistrictSelected () {
-
+  // options  subDistrict selected
+  async subDistrictSelected (selected) {
+    const { product, storeData } = this.props
+    const { districts, countProduct } = this.state
+    const params = {
+      id: product.id,
+      origin_id: storeData.district.id,
+      destination_id: districts.selected.id,
+      weight: countProduct * product.weight
+    }
+    await this.props.dispatch(expeditionActions.estimatedShipping(params))
+    this.setState({ subDistricts: { ...this.state.subDistricts, selected, show: false } })
   }
 
   // load any data
@@ -82,18 +98,37 @@ class ProductDetailServices extends Component {
   }
 
   componentWillReceiveProps (nextProps) {
-    const { provinces, districts, subDistricts } = nextProps
+    const { provinces, districts, subDistricts, estimatedCharges } = nextProps
 
     !provinces.isLoading && this.setState({ provinces: { ...this.state.provinces, data: provinces } })
 
     !districts.isLoading && this.setState({ districts: { ...this.state.districts, data: districts } })
 
     !subDistricts.isLoading && this.setState({ subDistricts: { ...this.state.subDistricts, data: subDistricts } })
+
+    if (!estimatedCharges.isLoading) {
+      switch (estimatedCharges.status) {
+        case Status.SUCCESS :
+          // (estimatedCharges.isFound)
+          // ?
+          this.setState({ estimatedCharges })
+          // : this.setState({ notification: {status: true, message: 'Data produk tidak ditemukan'} })
+          break
+        case Status.OFFLINE :
+        case Status.FAILED :
+          // this.setState({ notification: {status: true, message: estimatedCharges.message} })
+          break
+        default:
+          break
+      }
+    }
   }
+
+  formActivePress = () => this.setState({ formActive: true })
 
   render () {
     const { product } = this.props
-    let { countProduct, provinces, districts, subDistricts } = this.state
+    let { countProduct, provinces, districts, subDistricts, estimatedCharges, formActive } = this.state
     return (
       <Section className='section is-paddingless has-shadow'>
         <div className='container is-fluid'>
@@ -101,72 +136,80 @@ class ProductDetailServices extends Component {
             <h3>Estimasi Pengiriman</h3>
           </div>
         </div>
-        <div className='info-product'>
-          <div className='detail-rate'>
-            <div className='columns detail-rating is-mobile is-multiline no-margin-bottom'>
-              <div className='column is-half'>
-                <div className='rating-content is-left'>
-                  <strong>Jumlah Barang </strong>
+        {
+          !formActive
+          ? <div className='info-product'>
+            <div className='detail-rate'>
+              <div className='columns detail-rating is-mobile is-multiline no-margin-bottom'>
+                <div className='column'>
+                  <div className='rating-content see-detail-price'>
+                    <p>Lihat estimasi harga dan lama pengiriman barang ke tempat Anda</p>
+                    <a onClick={() => this.formActivePress()} className='button is-primary is-outlined is-thick is-fullwidth md'>Lihat Estimasi Pengiriman Barang</a>
+                  </div>
                 </div>
               </div>
-              <div className='column is-half'>
-                <div className='rating-content is-left has-text-centered'>
-                  <a><span className='icon-qty-min' onClick={() => (countProduct > 0) && this.minPress()} /></a>
-                  <span className='qty'>{ countProduct }</span>
-                  <a><span className='icon-qty-plus' onClick={() => (countProduct < product.stock) && this.plussPress()} /></a>
-                </div>
-              </div>
-            </div>
-            <div className='detail-result solid'>
-              <strong>Lokasi Pengiriman</strong>
-              <ul>
-                <li>
-                  <div className='detail-address'>
-                    <span className='label'>Provinsi:</span>
-                    <span onClick={() => this.provinceShow()} className='location-label js-option' data-target='#province'>{ provinces.selected.name || 'Pilih Provinsi' }</span>
-                  </div>
-                </li>
-                <li>
-                  <div className='detail-address'>
-                    <span className='label'>Kabupaten:</span>
-                    <span onClick={() => this.districtShow()} className='location-label js-option' data-target='#district'>{ districts.selected.name || 'Pilih Kabupaten' }</span>
-                  </div>
-                </li>
-                <li>
-                  <div className='detail-address'>
-                    <span className='label'>Kecamatan:</span>
-                    <span onClick={() => this.subDistrictShow()} className='location-label js-option' data-target='#districts'>{ subDistricts.selected.name || 'Pilih Kecamatan' }</span>
-                  </div>
-                </li>
-              </ul>
-            </div>
-            <div className='detail-result white'>
-              <ul>
-                <li>
-                  <div className='columns custom is-mobile'>
-                    <div className='column'><span>JNE REG</span></div>
-                    <div className='column has-text-right'><span>328.000</span></div>
-                    <div className='column has-text-right'><span>2-3 hari</span></div>
-                  </div>
-                </li>
-                <li>
-                  <div className='columns custom is-mobile'>
-                    <div className='column'><span>TIKI ONS</span></div>
-                    <div className='column has-text-right'><span>328.000</span></div>
-                    <div className='column has-text-right'><span>1-2 hari</span></div>
-                  </div>
-                </li>
-                <li>
-                  <div className='columns custom is-mobile'>
-                    <div className='column'><span>TIKI REG</span></div>
-                    <div className='column has-text-right'><span>328.000</span></div>
-                    <div className='column has-text-right'><span>2-4 hari</span></div>
-                  </div>
-                </li>
-              </ul>
             </div>
           </div>
-        </div>
+          : <div className='info-product'>
+            <div className='detail-rate'>
+              <div className='columns detail-rating is-mobile is-multiline no-margin-bottom'>
+                <div className='column is-half'>
+                  <div className='rating-content is-left'>
+                    <strong>Jumlah Barang </strong>
+                  </div>
+                </div>
+                <div className='column is-half'>
+                  <div className='rating-content is-left has-text-centered'>
+                    <a><span className='icon-qty-min' onClick={() => (countProduct > 0) && this.minPress()} /></a>
+                    <span className='qty'>{ countProduct }</span>
+                    <a><span className='icon-qty-plus' onClick={() => (countProduct < product.stock) && this.plussPress()} /></a>
+                  </div>
+                </div>
+              </div>
+              <div className='detail-result solid'>
+                <strong>Lokasi Pengiriman</strong>
+                <ul>
+                  <li>
+                    <div className='detail-address'>
+                      <span className='label'>Provinsi:</span>
+                      <span onClick={() => this.provinceShow()} className='location-label js-option' data-target='#province'>{ provinces.selected.name || 'Pilih Provinsi' }</span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className='detail-address'>
+                      <span className='label'>Kabupaten:</span>
+                      <span onClick={() => this.districtShow()} className='location-label js-option' data-target='#district'>{ districts.selected.name || 'Pilih Kabupaten' }</span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className='detail-address'>
+                      <span className='label'>Kecamatan:</span>
+                      <span onClick={() => this.subDistrictShow()} className='location-label js-option' data-target='#districts'>{ subDistricts.selected.name || 'Pilih Kecamatan' }</span>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              {
+                  estimatedCharges.isFound &&
+                  <div className='detail-result white'>
+                    <ul>
+                      {
+                          estimatedCharges.charges.map((charge, index) => {
+                            return <li key={index}>
+                              <div className='columns custom is-mobile'>
+                                <div className='column'><span>{ charge.full_name }</span></div>
+                                <div className='column has-text-right'><span>{ charge.cost }</span></div>
+                                <div className='column has-text-right'><span>{ charge.etd } hari</span></div>
+                              </div>
+                            </li>
+                          })
+                        }
+                    </ul>
+                    </div>
+                }
+            </div>
+          </div>
+        }
 
         <OptionsProvince
           {...provinces}
@@ -191,7 +234,7 @@ const OptionsProvince = (props) => {
         <p><strong>Pilih Provinsi</strong></p>
         <form action='#' className='form'>
           <div className='field'>
-            <div className='control province-option' style={{ height: '500px', overflow: 'scroll' }}>
+            <div className='control popup-option' style={{ height: '500px', overflow: 'scroll' }}>
               {
                 props.data.provinces.map((province) => {
                   return (
@@ -217,7 +260,7 @@ const OptionsDistritcs = (props) => {
         <p><strong>Pilih Kabupaten</strong></p>
         <form action='#' className='form'>
           <div className='field'>
-            <div className='control province-option' style={{ height: '500px', overflow: 'scroll' }}>
+            <div className='control popup-option' style={{ height: '500px', overflow: 'scroll' }}>
               {
                 props.data &&
                 props.data.districts.map((district) => {
@@ -244,7 +287,7 @@ const OptionsSubDistritcs = (props) => {
         <p><strong>Pilih Kecamatan</strong></p>
         <form action='#' className='form'>
           <div className='field'>
-            <div className='control province-option' style={{ height: '500px', overflow: 'scroll' }}>
+            <div className='control popup-option' style={{ height: '500px', overflow: 'scroll' }}>
               {
                 props.data &&
                 props.data.subdistricts.map((subDistrict) => {
@@ -268,7 +311,8 @@ const mapStateToProps = (state) => {
   return {
     provinces: state.provinces,
     districts: state.districts,
-    subDistricts: state.subdistricts
+    subDistricts: state.subdistricts,
+    estimatedCharges: state.estimatedCharges
   }
 }
 
