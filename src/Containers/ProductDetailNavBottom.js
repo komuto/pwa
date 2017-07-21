667949ffa4e97ccb0431823b052b273424721387
safei muslim <safeydev@gmail.com>
2017-07-21 15:35:23 +0700

    add event Diskusi button

--- src/Containers/ProductDetailNavBottom.js
+++ src/Containers/ProductDetailNavBottom.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import Router from 'next/router'
 
 class ProductDetailNavBottom extends Component {
   render () {
+    const props = this.props
     return (
       <div className='level nav-bottom nav-button is-mobile'>
-        <a className='button is-m-lg is-fullwidth is-outlined'><span className='icon-comment black' /> Diskusi (278)</a>
+        <a className='button is-m-lg is-fullwidth is-outlined'
+          onClick={() => {
+            Router.push(`/discussion?id=${props.id}`)
+          }}><span className='icon-comment black' /> Diskusi ({props.count_discussion})</a>
         <a className='button is-primary is-m-lg is-fullwidth'>Beli Sekarang</a>
       </div>
     )
