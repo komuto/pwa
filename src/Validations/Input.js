d4a7ca222b873e9bcf41f1fdbc2e4160edaffbdb
safei muslim <safeydev@gmail.com>
2017-07-28 15:51:22 +0700

    add validation input

---
+++ src/Validations/Input.js
@@ -0,0 +1,18 @@
+
+const inputNormal = (input) => {
+  return input.replace(/[^a-zA-Z0-9 ]/g, '')
+}
+
+const inputString = (input) => {
+  return input.replace(/[^a-zA-Z ]/g, '')
+}
+
+const inputNumber = (input) => {
+  return input.replace(/[^0-9]/g, '')
+}
+
+export {
+  inputNormal,
+  inputString,
+  inputNumber
+}
