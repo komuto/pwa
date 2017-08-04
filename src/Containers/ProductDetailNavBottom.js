b56b63a0bf80031183a8a334304ce5b193bf10f7
safei muslim <safeydev@gmail.com>
2017-08-04 08:21:25 +0700

    event button navbar bottom

--- src/Containers/ProductDetailNavBottom.js
+++ src/Containers/ProductDetailNavBottom.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
-import Router from 'next/router'
 
 class ProductDetailNavBottom extends Component {
   render () {
     const props = this.props
     return (
       <div className='level nav-bottom nav-button is-mobile'>
-        <a className='button is-m-lg is-fullwidth is-outlined'
-          onClick={() => {
-            Router.push(`/discussion?id=${props.id}`)
-          }}><span className='icon-comment black' /> Diskusi ({props.count_discussion})</a>
-        <a className='button is-primary is-m-lg is-fullwidth'
-          onClick={() => Router.push(`/purchase?id=${props.id}`)}>Beli Sekarang</a>
+        <a className={`button is-m-lg is-fullwidth is-outlined ${props.submitingDiscussion && 'is-loading'}`}
+          onClick={() => { !props.submitingDiscussion && props.discussion() }}>
+          { !props.submitingDiscussion && <span className='icon-comment black' /> }
+          Diskusi ({props.count_discussion})
+        </a>
+        <a className={`button is-primary is-m-lg is-fullwidth ${props.submiting && 'is-loading'}`}
+          onClick={() => !props.submiting && props.purchaseNow()}>Beli Sekarang</a>
       </div>
     )
   }
