424a058589332b391b75ba828ed64be2a4437380
safei muslim <safeydev@gmail.com>
2017-08-04 18:34:59 +0700

    update view shoppingg cart

--- src/Containers/ShoppingCart.js
+++ src/Containers/ShoppingCart.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Router from 'next/router'
 // component
 import Section from '../Components/Section'
-import Content from '../Components/Content'
+// import Content from '../Components/Content'
 import MyImage from '../Components/MyImage'
 // actions
 import * as cartActions from '../actions/cart'
@@ -16,7 +17,6 @@ class ShoppingCart extends Component {
     super(props)
     this.state = {
       cart: props.cart || null,
-      itemSelected: null,
       notification: {
         type: 'is-danger',
         status: false,
@@ -25,8 +25,28 @@ class ShoppingCart extends Component {
     }
   }
 
-  itemSelected (selected) {
-    this.setState({ itemSelected: (this.state.itemSelected === selected) ? null : selected })
+  minPress (myItem) {
+    let { cart } = this.state
+    // console.log(cart)
+    // console.log(myItem)
+    cart.cart.items.map((item) => {
+      if (item.id === myItem.id && item.qty > 1) {
+        item.qty -= 1
+      }
+    })
+
+    this.setState({ cart })
+  }
+
+  plusPress (myItem) {
+    let { cart } = this.state
+    cart.cart.items.map((item) => {
+      if (item.id === myItem.id && item.qty < 100) {
+        item.qty += 1
+      }
+    })
+
+    this.setState({ cart })
   }
 
   async componentDidMount () {
@@ -55,192 +75,79 @@ class ShoppingCart extends Component {
   }
 
   render () {
-    const { cart, itemSelected } = this.state
+    const { cart } = this.state
     if (!cart.isFound) return null
-    console.log(cart)
-    console.log(itemSelected)
+    // console.log(cart)
     return (
       <Section>
-        <section className='section is-paddingless has-shadow'>
-          <div className='detail-product'>
-            <div className='purchase'>
-              <figure className='img-item'>
-                <img src='../images/pict.jpg' alt='pict' />
-              </figure>
-              <h3>Blue Training Kit Machester United</h3>
-              <span className='price'>Sport Station Shop</span>
-            </div>
-            <a className='remove-item'>Hapus</a>
-          </div>
-          <div className='info-purchase'>
-            <div className='detail-rate is-purchase'>
-              <div className='columns total-items is-mobile is-multiline no-margin-bottom'>
-                <div className='column is-half'>
-                  <div className='rating-content is-left'>
-                    <strong>Harga Satuan</strong>
-                  </div>
-                </div>
-                <div className='column is-half'>
-                  <div className='rating-content item-qty has-text-right'>
-                    <span>Rp 135.000</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className='info-purchase'>
-            <div className='detail-rate is-purchase'>
-              <div className='columns detail-rating is-mobile is-multiline no-margin-bottom'>
-                <div className='column is-half is-paddingless'>
-                  <div className='rating-content is-left'>
-                    <strong>Jumlah</strong>
-                  </div>
-                </div>
-                <div className='column is-half is-paddingless'>
-                  <div className='rating-content item-qty'>
-                    <a><span className='icon-qty-min' /></a>
-                    <span className='qty'>1</span>
-                    <a><span className='icon-qty-plus' /></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className='see-all'>
-            <span className='link'>Detail Pengiriman <span className='icon-arrow-right' /></span>
-          </div>
-          <div className='info-purchase'>
-            <div className='detail-rate is-purchase'>
-              <div className='columns total-items is-mobile is-multiline no-margin-bottom'>
-                <div className='column is-half'>
-                  <div className='rating-content is-left'>
-                    <strong>Subtotal</strong>
-                  </div>
-                </div>
-                <div className='column is-half'>
-                  <div className='rating-content item-qty has-text-right'>
-                    <span>Rp 270.000</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
         {
           cart.cart.items.map((item) => {
-            let selected = itemSelected === item.id
             return (
-              <Content key={item.id}>
-                <div className='detail-product' onClick={() => this.itemSelected(item.id)}>
+              <section className='section is-paddingless has-shadow' key={item.id}>
+                <div className='detail-product'>
                   <div className='purchase'>
-                    <figure className='img-item'>
-                      <MyImage src={item.product.image} />
+                    <figure className='img-item' style={{ width: 50 }}>
+                      <MyImage src={item.product.image} alt='pict' />
                     </figure>
                     <h3>{item.product.name}</h3>
-                    <span className='price'>Rp {RupiahFormat(item.product.price)}</span>
+                    <span className='price'>{item.product.store.name}</span>
                   </div>
-                  <span className='remove-item'> <a>Hapus</a> <span className={selected ? 'icon-arrow-down' : 'icon-arrow-right'} /></span>
+                  <a className='remove-item'>Hapus</a>
                 </div>
-                <div style={{ display: selected ? 'block' : 'none' }}>
-                  <div className='info-purchase'>
-                    <div className='detail-rate is-purchase'>
-                      <div className='columns detail-rating is-mobile is-multiline no-margin-bottom'>
-                        <div className='column is-half is-paddingless'>
-                          <div className='rating-content is-left'>
-                            <strong>Jumlah</strong>
-                          </div>
+                <div className='info-purchase'>
+                  <div className='detail-rate is-purchase'>
+                    <div className='columns total-items is-mobile is-multiline no-margin-bottom'>
+                      <div className='column is-half'>
+                        <div className='rating-content is-left'>
+                          <strong>Harga Satuan</strong>
                         </div>
-                        <div className='column is-half is-paddingless'>
-                          <div className='rating-content item-qty'>
-                            <a><span className='icon-qty-min' /></a>
-                            <span className='qty'>1</span>
-                            <a><span className='icon-qty-plus' /></a>
-                          </div>
+                      </div>
+                      <div className='column is-half'>
+                        <div className='rating-content item-qty has-text-right'>
+                          <span>Rp {RupiahFormat(item.product.price)}</span>
                         </div>
                       </div>
                     </div>
                   </div>
-                  <div className='detail-purchase summary at-cart'>
-                    <div className='detail-result white'>
-                      <a className='btn-change js-option' data-target='#changeAddress'>Ganti</a>
-                      <ul className='data-delivery'>
-                        <li>
-                          <div className='columns custom is-mobile'>
-                            <div className='column'>
-                              <strong>Alamat Pengirim</strong>
-                              <span>
-                                Kemanggisan Jakarta Barat,<br />
-                                Palmerah<br />
-                                Jakarta Barat<br />
-                                DKI Jakarta, Indonesia 55673
-                              </span>
-                            </div>
-                          </div>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div className='column is-paddingless'>
-                    <div className='see-all'>
-                      <span className='link black js-option' data-target='#kurir'>Kurir Pengiriman <span className='kurir'>JNE</span><span className='icon-arrow-down' /></span>
-                    </div>
-                    <p className='error-msg'>Mohon Pilih Kurir Pengiriman terlebih dahulu</p>
-                  </div>
-
-                  <div className='column is-paddingless'>
-                    <div className='see-all'>
-                      <span className='link black js-option' data-target='#deliveryPackage'>Pilih Paket Pengiriman <span className='kurir'>Reguler</span><span className='icon-arrow-down' /></span>
-                    </div>
-                    <p className='error-msg'>Mohon Pilih Paket Pengiriman terlebih dahulu</p>
-                  </div>
-
-                  <div className='column is-paddingless'>
-                    <div className='see-all'>
-                      <span className='link black js-option' data-target='#insurance'>Asuransi <span className='kurir'>Tidak</span><span className='icon-arrow-down' /></span>
-                    </div>
-                    <p className='error-msg'>Mohon Pilih Paket Pengiriman terlebih dahulu</p>
-                  </div>
-
-                  <div className='info-purchase'>
-                    <div className='detail-purchase remark'>
-                      <h3>Catatan</h3>
-                      <p className='note-remark'>Saya pesan barang yang warna merah</p>
-                    </div>
-                  </div>
-
-                  <div className='detail-purchase summary'>
-                    <div className='detail-result detail-price'>
-                      <h3>Rincian Harga</h3>
-                      <ul>
-                        <li>
-                          <div className='columns custom is-mobile'>
-                            <div className='column is-half'><span>Harga Barang</span></div>
-                            <div className='column is-half has-text-right'><span>Rp 380.000</span></div>
-                          </div>
-                        </li>
-                        <li>
-                          <div className='columns custom is-mobile'>
-                            <div className='column is-half'><span>Biaya Asuransi</span></div>
-                            <div className='column is-half has-text-right'><span>Rp 10.000</span></div>
-                          </div>
-                        </li>
-                        <li>
-                          <div className='columns custom is-mobile'>
-                            <div className='column is-half'><span>Ongkos Kirim</span></div>
-                            <div className='column is-half has-text-right'><span>Rp 8.000</span></div>
-                          </div>
-                        </li>
-                        <li>
-                          <div className='columns custom is-mobile'>
-                            <div className='column is-half'><strong>Harga Barang</strong></div>
-                            <div className='column is-half has-text-right'><strong>Rp 380.000</strong></div>
-                          </div>
-                        </li>
-                      </ul>
+                </div>
+                <div className='info-purchase'>
+                  <div className='detail-rate is-purchase'>
+                    <div className='columns detail-rating is-mobile is-multiline no-margin-bottom'>
+                      <div className='column is-half is-paddingless'>
+                        <div className='rating-content is-left'>
+                          <strong>Jumlah</strong>
+                        </div>
+                      </div>
+                      <div className='column is-half is-paddingless'>
+                        <div className='rating-content item-qty'>
+                          <a onClick={() => this.minPress(item)}><span className='icon-qty-min' /></a>
+                          <span className='qty'>{item.qty}</span>
+                          <a onClick={() => this.plusPress(item)}><span className='icon-qty-plus' /></a>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </Content>
+                <div className='see-all' onClick={() => Router.push('/shipping-detail?id=' + item.id)}>
+                  <span className='link'>Detail Pengiriman <span className='icon-arrow-right' /></span>
+                </div>
+                <div className='info-purchase'>
+                  <div className='detail-rate is-purchase'>
+                    <div className='columns total-items is-mobile is-multiline no-margin-bottom'>
+                      <div className='column is-half'>
+                        <div className='rating-content is-left'>
+                          <strong>Subtotal</strong>
+                        </div>
+                      </div>
+                      <div className='column is-half'>
+                        <div className='rating-content item-qty has-text-right'>
+                          <span>Rp {RupiahFormat(item.total_price)}</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
             )
           })
         }
