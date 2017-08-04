02674474ee5bf84aac0772d3df4568ab1bf16b0b
safei muslim <safeydev@gmail.com>
2017-08-04 08:18:54 +0700

    - add input validaitions - add debounce

--- src/Containers/MySearch.js
+++ src/Containers/MySearch.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Router from 'next/router'
 import url from 'url'
-// import NProgress from 'nprogress'
 // import _ from 'lodash'
 // actions
 import * as homeActions from '../actions/home'
@@ -10,31 +9,31 @@ import * as homeActions from '../actions/home'
 import Notification from '../Components/Notification'
 // utils
 import { Status } from '../Services/Status'
+// validations
+import * as inputValidations from '../Validations/Input'
 
 class MySearch extends Component {
   constructor (props) {
     super(props)
     this.state = {
       searchProduct: props.searchProduct || [],
-      searchValue: '',
+      submitting: false,
       notification: {
         status: false,
         message: 'Error, default message.'
       }
     }
-
-    this.onChange = this.onChange.bind(this)
   }
 
-  onChange (event) {
-    const searchValue = event.target.value.replace(/[^a-zA-Z0-9 ]/g, '')
-
-    if (searchValue !== '') {
-      // _.debounce(() => {
-      this.props.dispatch(homeActions.search({query: searchValue}))
-      // }, 1000)
-    }
-    this.setState({ searchValue })
+  doSearch (evt) {
+    var searchText = inputValidations.inputNormal(evt.target.value) // this is the search text
+    // _.debounce(() => {
+    //    console.log(searchText)
+    // }, 500)
+    if (this.timeout) clearTimeout(this.timeout)
+    this.timeout = setTimeout(() => {
+      searchText && this.props.dispatch(homeActions.search({query: searchText})) && this.setState({ submitting: true })
+    }, 1000)
   }
 
   componentWillReceiveProps (nextProps) {
@@ -58,19 +57,20 @@ class MySearch extends Component {
       }
     }
 
-    this.setState({ searchProduct, notification })
+    this.setState({ searchProduct, submitting: false, notification })
   }
 
   render () {
-    const { searchValue, searchProduct, notification } = this.state
+    const { submitting, searchProduct, notification } = this.state
     let key = 0
     return (
       <div className='on-search slide'>
         <div className='header-search'>
           <a className='back back-on-seacrh' onClick={() => Router.back()}><span className='icon-arrow-left black' /></a>
           <div className='field'>
-            <p className='control has-icons-left'>
-              <input className='input is-medium' type='text' placeholder='Cari barang atau toko' onChange={(event) => this.onChange(event)} value={searchValue} />
+            <p className={`control has-icons-left`}>
+              <span className={`${!submitting && 'button self is-loading right'}`} />
+              <input className='input is-medium' type='text' placeholder='Cari barang atau toko' onChange={evt => this.doSearch(evt)} />
             </p>
           </div>
         </div>
