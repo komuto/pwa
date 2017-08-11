857c6369177c2160fa762cb06289351f1e2d732b
edimdendidiwangga <edimdendy@gmail.com>
2017-08-11 19:59:30 +0700

    adding stores

--- src/reducers/stores.js
+++ src/reducers/stores.js
@@ -30,6 +30,35 @@ const initStoreProducts = {
   ...initState()
 }
 
+const initProcessCreateStore = {
+  store: {
+    name: '',
+    slogan: '',
+    description: '',
+    logo: '',
+    path: ''
+  },
+  expedition_services: {
+    selectedExpeditions: [],
+    selectedServices: []
+  },
+  user: {
+    id_number: '',
+    mother_name: ''
+  },
+  address: {
+    province_id: null,
+    district_id: null,
+    sub_district_id: null,
+    village_id: null,
+    name: '',
+    email: '',
+    phone_number: '',
+    postal_code: '',
+    address: ''
+  }
+}
+
 function stores (state = initStore, action) {
   const type = buildType(action.type)
   switch (type) {
@@ -132,7 +161,39 @@ export const getStoreCatalogProducts = (state = initState({ storeCatalogProducts
   }
 }
 
+const processCreateStore = (state = initProcessCreateStore, action) => {
+  switch (action.type) {
+    case actions.INFO_STORE: {
+      return {
+        ...state,
+        store: action.params
+      }
+    }
+    case actions.SHIPPING_EXPEDITION: {
+      return {
+        ...state,
+        expedition_services: action.params
+      }
+    }
+    case actions.OWNER_INFO: {
+      return {
+        ...state,
+        user: action.params
+      }
+    }
+    case actions.ADDRESS_INFO: {
+      return {
+        ...state,
+        address: action.params
+      }
+    }
+    default:
+      return state
+  }
+}
+
 export {
+    processCreateStore,
     stores,
     photoUpload,
     createStore,
