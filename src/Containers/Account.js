0320b360de711eb924aab828b8aea088c2c4f093
edimdendidiwangga <edimdendy@gmail.com>
2017-08-11 19:43:53 +0700

    changes Container/Account

--- src/Containers/Account.js
+++ src/Containers/Account.js
@@ -7,7 +7,7 @@ import NProgress from 'nprogress'
 import Content from '../Components/Content'
 import Section from '../Components/Section'
 // import { ButtonFullWidth } from '../Components/Button'
-import Link from 'next/link'
+import Router from 'next/router'
 import {Images} from '../Themes'
 // actions
 import * as loginAction from '../actions/user'
@@ -30,13 +30,102 @@ class Account extends Component {
   }
 
   componentWillReceiveProps (nextProps) {
-    // const { user } = nextProps
-    // const data = user
+    if (nextProps.sendOTPPhone.status === 200) {
+      Router.push('/verify-no-telp')
+    }
+  }
+
+  sendOTPPhone (e) {
+    e.preventDefault()
+    this.props.sendOTPToPhone()
+  }
+
+  renderModalVerify () {
+    const { verify } = this.state
+    return (
+      <div className='sort-option' style={{display: verify && 'block'}}>
+        <div className='notif-report'>
+          <img src={Images.unverifiedPhone} alt='' />
+          <h3>Anda Belum Memverifikasi Nomor Telepon</h3>
+          <p>Verifikasi Nomor Telepon Anda terlebih dahulu untuk melanjutkan proses membuka toko</p>
+          <button
+            onClick={(e) => this.sendOTPPhone(e)}
+            className='button is-primary is-large is-fullwidth'>
+            Verifikasi Sekarang
+          </button>
+          <strong className='cancel' onClick={() => this.handleVerify()} style={{color: '#56aaef', padding: '20px', display: 'block', marginTop: '10px'}}>Batal</strong>
+        </div>
+      </div>
+    )
+  }
+
+  renderStore () {
+    const { user, profile } = this.props
+    if (profile.user.store.hasOwnProperty('name')) {
+      return (
+        <Section className='bg-white'>
+          <div className='profile-wrapp'>
+            <ul>
+              <li>
+                <div className='box is-paddingless'>
+                  <article className='media'>
+                    <div className='media-left'>
+                      <figure className='image'>
+                        <img src={profile.user.store.logo} />
+                      </figure>
+                    </div>
+                    <div className='media-content'>
+                      <div className='content'>
+                        <p>
+                          <strong>{profile.user.store.name}</strong><br />
+                          Kelola Toko Anda
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                </div>
+                <span className='icon-arrow-right' />
+              </li>
+            </ul>
+          </div>
+        </Section>
+      )
+    } else {
+      return (
+        <Section className='bg-white'>
+          <div className='profile-wrapp'>
+            <ul>
+              <li
+                onClick={() => user.is_phone_verified ? Router.push('/add-information-store') : this.handleVerify()}>
+                <div className='box is-paddingless'>
+                  <article className='media'>
+                    <div className='media-left'>
+                      <figure className='image'>
+                        <span className='icon-toko' />
+                      </figure>
+                    </div>
+                    <div className='media-content'>
+                      <div className='content'>
+                        <p>
+                          <strong>Anda belum memiliki Toko</strong><br />
+                          Buat Sekarang
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                </div>
+                <span className='icon-arrow-right' />
+              </li>
+            </ul>
+          </div>
+        </Section>
+      )
+    }
   }
 
   render () {
-    const { user } = this.props
-    const { verify } = this.state
+    const { user, profile } = this.props
+    console.log('profile ', profile)
     return (
       <Content>
         <Section className='bg-white'>
@@ -130,51 +219,26 @@ class Account extends Component {
             </ul>
           </div>
         </Section>
-        <Section className='bg-white'>
-          <div className='profile-wrapp'>
-            <ul>
-              <li>
-                <div className='box is-paddingless' onClick={() => this.handleVerify()}>
-                  <article className='media'>
-                    <div className='media-left'>
-                      <figure className='image'>
-                        <span className='icon-toko' />
-                      </figure>
-                    </div>
-                    <div className='media-content'>
-                      <div className='content'>
-                        <p>
-                          <strong>Anda belum memiliki Toko</strong><br />
-                          Buat Sekarang
-                        </p>
-                      </div>
-                    </div>
-                  </article>
-                </div>
-                <span className='icon-arrow-right' />
-              </li>
-            </ul>
-          </div>
-        </Section>
-        <div className='sort-option' style={{display: verify && 'block'}}>
-          <div className='notif-report'>
-            <img src={Images.unverifiedPhone} alt='' />
-            <h3>Anda Belum Memverifikasi Nomor Telepon</h3>
-            <p>Verifikasi Nomor Telepon Anda terlebih dahulu untuk melanjutkan proses membuka toko</p>
-            <Link href='verify-no-telp' as='v'>
-              <button className='button is-primary is-large is-fullwidth'>Verifikasi Sekarang</button>
-            </Link>
-            <strong className='cancel' onClick={() => this.handleVerify()} style={{color: '#56aaef', padding: '20px', display: 'block', marginTop: '10px'}}>Batal</strong>
-          </div>
-        </div>
+        {this.renderStore()}
+        {this.renderModalVerify()}
       </Content>
     )
   }
 }
+
 const mapStateToProps = (state) => {
   return {
     // user: state.user
+    users: state.user,
+    sendOTPPhone: state.sendOTPPhone,
+    profile: state.profile
   }
 }
 
-export default connect(mapStateToProps)(Account)
+const mapDispatchToProps = dispatch => ({
+  sendOTPToPhone: () => dispatch(loginAction.sendOTPPhone()),
+  getProfile: () => dispatch(loginAction.getProfile())
+  // verifyStore: () => dispatch(verifyStore())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Account)
