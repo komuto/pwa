4886422a1d1a0f9b07470af2d9997d65901354e0
edimdendidiwangga <edimdendy@gmail.com>
2017-08-12 08:49:27 +0700

    add pages/has-opened-store (seller buka toko)

--- pages/has-opened-store.js
+++ pages/has-opened-store.js
@@ -6,20 +6,12 @@ import ReduxPageWrapper from '../src/Utils/ReduxPageWrapper'
 // containers
 import HasOpenedStore from '../src/Containers/HasOpenedStore'
 // utils
-import { PROFILE } from '../src/Utils/Constant'
+// import { PROFILE } from '../src/Utils/Constant'
 
 const params = {
   style: 'main user user-success',
   header: {
     title: 'Berhasil Membuka Toko'
-  },
-  navbar: {
-    searchBoox: false,
-    textPath: ''
-  },
-  tabbar: {
-    active: PROFILE,
-    isShow: true
   }
 }
 
