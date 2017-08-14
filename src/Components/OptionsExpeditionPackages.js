4b6d2c5da2dd19b35bae70bb63b167dfcb1550a3
safei muslim <safeydev@gmail.com>
2017-08-14 11:54:11 +0700

    add filter service by expeditions selected

--- src/Components/OptionsExpeditionPackages.js
+++ src/Components/OptionsExpeditionPackages.js
@@ -2,17 +2,22 @@
 import RupiahFormat from '../Lib/RupiahFormat'
 
 const OptionsExpeditionPackages = (props) => {
+  console.log('props', props)
+  let epFilterByExpeditions = props.expeditionsPackage.data.filter((data) => {
+    return data.expedition_id === props.expeditions.selected.id
+  })
+  console.log('epFilterByExpeditions', epFilterByExpeditions)
   return (
-    <div className='sort-option' id='deliveryPackage' style={{display: props.show && 'block'}}>
+    <div className='sort-option' id='deliveryPackage' style={{display: props.expeditionsPackage.show && 'block'}}>
       <div className='sort-list'>
-        <p><strong>Pilih Paket Pengiriman dari JNE</strong></p>
+        <p><strong>Pilih Paket Pengiriman dari {props.expeditions.selected.name} </strong></p>
         <form action='#' className='form'>
           <div className='field'>
             <div className='control popup-option package' style={{ height: '500px', overflow: 'scroll' }}>
               {
-                props.data.map((data) => {
+                epFilterByExpeditions.map((data) => {
                   return (
-                    <label className={`radio ${props.selected.id === data.id && 'checked'}`} key={data.id} onClick={(e) => props.expeditionsPackageSelected(e, data)}>
+                    <label className={`radio ${props.expeditionsPackage.selected.id === data.id && 'checked'}`} key={data.id} onClick={(e) => props.expeditionsPackageSelected(e, data)}>
                       <input type='radio' name='reguler' />
                       <span className='service-name'>{ data.full_name }</span>
                       <span>{data.etd} Hari</span>
@@ -20,6 +25,13 @@ const OptionsExpeditionPackages = (props) => {
                     </label>
                   )
                 })
+              }
+              {
+                epFilterByExpeditions.length < 1 &&
+                <label className={`radio`}>
+                  <input type='radio' name='reguler' />
+                  <span className='service-name'>Service tidak ditemukan</span>
+                </label>
               }
             </div>
           </div>
