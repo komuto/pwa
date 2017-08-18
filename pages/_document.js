3bae3a9fa6014e6a592e567e29ed90c8f3313787
safei muslim <safeydev@gmail.com>
2017-08-18 20:48:01 +0700

    add DOKU setting

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,13 @@ export default class HomeDocument extends Document {
           <title>Komuto</title>
           <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport' />
           <meta name='apple-mobile-web-app-status-bar-style' content='black' />
+          <script src='https://code.jquery.com/jquery-2.1.4.min.js' />
+          <script src='https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.pack.js' />
+          {/* <script src="https://staging.doku.com/doku-js/assets/js/doku.js" /> */}
+          <script src={`${AppConfig.baseURL}static/js/MyDoku.js`} />
+          <script src='https://github.com/jackspirou/clientjs/blob/master/dist/client.min.js' />
+          {/* <link rel='stylesheet prefetch' href="http://staging.doku.com/doku-js/assets/css/doku.css" /> */}
+          {/* <link href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" rel="stylesheet" /> */}
           {/* Chrome, Firefox OS and Opera */}
           <meta name='theme-color' content='#ef5656' />
           {/* Windows Phone */}
@@ -28,7 +35,8 @@ export default class HomeDocument extends Document {
           <Main />
           <script src={`${AppConfig.baseURL}static/js/index.js`} />
           <NextScript />
-          <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js' />
+          {/* <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js' />
+          <script src='http://staging.doku.com/doku-js/assets/js/doku.js' /> */}
           <script src={`${AppConfig.baseURL}static/js/nprogress.js`} />
         </body>
       </html>
