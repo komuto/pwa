01cd128053c420307d4699f9eead672e0b2b4d5a
safei muslim <safeydev@gmail.com>
2017-08-18 20:55:29 +0700

    add effect styles

--- src/Containers/Header.js
+++ src/Containers/Header.js
@@ -12,9 +12,11 @@ const Header = (props) => {
       <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport' />
       <meta name='apple-mobile-web-app-status-bar-style' content='black' />
       <link rel='stylesheet prefetch' href={`${AppConfig.baseURL}static/dist/css/style.css`} />
+      <link rel='stylesheet prefetch' href={`${AppConfig.baseURL}static/dist/css/effect.css`} />
       <link rel='stylesheet prefetch' href={`${AppConfig.baseURL}static/dist/css/nprogress.css`} />
       <link rel='stylesheet' type='text/css' href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css' />
       <link rel='stylesheet' type='text/css' href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css' />
+
       {/* Chrome, Firefox OS and Opera */}
       <meta name='theme-color' content='#ef5656' />
       {/* Windows Phone */}
