2bf16177d18adb7c5c3053a3ef9f677cbc509a92
safei muslim <safeydev@gmail.com>
2017-08-23 14:03:02 +0700

    create form doku

--- src/Containers/PaymentDoku.js
+++ src/Containers/PaymentDoku.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import NProgress from 'nprogress'
 // component
-import Content from '../Components/Content'
+// import Content from '../Components/Content'
 // actios
 import * as paymentActions from '../actions/payment'
 import * as cartActions from '../actions/cart'
+// import AppConfig from '../Config/AppConfig'
 // services
 import { Status } from '../Services/Status'
+// import '../../static/dist/css/doku.css'
 
 class PaymentDoku extends Component {
   constructor (props) {
@@ -22,6 +24,8 @@ class PaymentDoku extends Component {
         message: 'Error, default message.'
       }
     }
+
+    this.dokuIsRendered = false
   }
 
   async componentDidMount () {
@@ -31,6 +35,7 @@ class PaymentDoku extends Component {
       await this.props.getCart()
     }
     if (!invoice.isFound) {
+      console.log('Load Invoice')
       NProgress.start()
       await this.props.getDokuInvoice()
     }
@@ -60,7 +65,24 @@ class PaymentDoku extends Component {
       NProgress.done()
       switch (invoice.status) {
         case Status.SUCCESS :
-          if (!invoice.isFound) notification = {type: 'is-danger', status: true, message: 'Konfirmasi metode pembayaran gagal!'}
+          if (!invoice.isFound) {
+            notification = {type: 'is-danger', status: true, message: 'Konfirmasi metode pembayaran gagal!'}
+          } else {
+            if (typeof getForm !== 'undefined' && invoice.isFound && !this.dokuIsRendered) {
+              var data = {
+                req_merchant_code: '4533',
+                req_chain_merchant: 'NA',
+                req_payment_channel: '04',
+                req_transaction_id: invoice.invoice.invoice,
+                req_currency: '360',
+                req_amount: invoice.invoice.amount,
+                req_words: invoice.invoice.words,
+                req_form_type: 'full'
+              }
+              this.dokuIsRendered = true
+              getForm(data)
+            }
+          }
           break
         case Status.OFFLINE :
         case Status.FAILED :
@@ -75,133 +97,12 @@ class PaymentDoku extends Component {
 
   render () {
     const { invoice } = this.state
-    if (typeof getForm !== 'undefined') {
-      let data = {
-        req_merchant_code: '4533', // mall id or merchant id
-        req_chain_merchant: invoice.invoice.chain_merchant, // chain merchant id
-        req_payment_channel: '04', // payment channel
-        req_transaction_id: invoice.invoice.invoice, // invoice no
-        req_amount: invoice.invoice.amount,
-        req_currency: invoice.invoice.currency, // 360 for IDR
-        req_words: invoice.invoice.words, // your merchant unique key
-        req_session_id: new Date().getTime(), // your server timestamp
-        req_form_type: 'full'
-      }
-      getForm(data)
-    }
-
+    if (!invoice.isFound) return null
     return (
-      <Content>
-        <div id='formPayment' />
-        {/* <section className="section is-paddingless has-shadow">
-          <div className="columns is-mobile no-margin-bottom notif-pay bg-white">
-            <div className="column">
-              <div className="is-left">
-                <ul className="list-inline col2w">
-                  <li className="label-text">
-                    <span>Akun Doku Wallet Anda</span>
-                  </li>
-                  <li>
-                    <div className="has-text-right img-method">
-                      <img src="../images/norton.png" alt="" />
-                      <img src="../images/doku-wallet.png" alt="" />
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div className="columns is-mobile no-margin-bottom notif-pay bg-white">
-            <div className="column">
-              <div className="form-pay">
-                <div className="field">
-                  <p className="control">
-                    <input className="input" type="text" placeholder="User ID" />
-                  </p>
-                  <p class="control">
-                    <input className="input" type="text" placeholder="Password" />
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section className="section is-paddingless has-shadow">
-          <div className="payment-detail step-pay">
-            <ul>
-              <li>
-                <div className="columns is-mobile is-multiline no-margin-bottom">
-                  <div className="column">
-                    <div className="label-text is-left">
-                      <span>Rincian Harga</span>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <ul className="total-pay">
-                  <li>
-                    <div className="columns is-mobile is-multiline no-margin-bottom">
-                      <div className="column">
-                        <div className="label-text is-left">
-                          <span>
-                            Total Belanja
-                          </span>
-                        </div>
-                      </div>
-                      <div className="column is-one-third">
-                        <div className="has-text-right">
-                          <span>Rp 320.000</span>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="columns is-mobile is-multiline no-margin-bottom">
-                      <div className="column">
-                        <div className="label-text is-left">
-                          <span className="pay-code">
-                            Kode Voucher BELANJAENAK
-                          </span>
-                        </div>
-                      </div>
-                      <div className="column is-one-third">
-                        <div className="has-text-right">
-                          <span className="pay-code"> - Rp 20.000</span>
-                        </div>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="columns is-mobile is-multiline no-margin-bottom">
-                      <div className="column">
-                        <div className="label-text is-left">
-                          <span>
-                            Sisa Pembayaran
-                          </span>
-                        </div>
-                      </div>
-                      <div className="column is-one-third">
-                        <div className="has-text-right">
-                          <span>Rp 250.219</span>
-                        </div>
-                      </div>
-                    </div>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </section>
-        <section className="section is-paddingless">
-          <div className="payment-detail action">
-            <ul>
-              <li>
-                <span>Dengan menekan tombol "Lanjutkan" Anda telah menyetujui Syarat dan Ketentuan dari Komuto</span>
-                <a className="button is-primary is-large is-fullwidth">Proses Pembayaran</a>
-              </li>
-            </ul>
-          </div>
-        </section> */}
-      </Content>
+      <div id='formPayment' style={{ marginTop: 10 }}>
+        <input id='doku-token' name='doku-token' type='hidden' />
+        <input id=' doku-pairing-code' name='doku-pairing-code' type='hidden' />
+      </div>
     )
   }
 }
