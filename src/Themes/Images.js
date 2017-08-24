96fea6a298ee55d46554223c3202ea38cc97721d
edimdendidiwangga <edimdendy@gmail.com>
2017-08-24 14:58:08 +0700

    edit initial id in api getBankAccounts

--- src/Themes/Images.js
+++ src/Themes/Images.js
@@ -1,6 +1,7 @@
 const images = {
   about: 'https://skyshi.com/sites/skyshi.com/files/logo_rev_1.png',
   password: '../../static/images/password.svg',
+  phoneSuccess: '../../static/images/phone.png',
   phoneAccount: '../../static/images/phone-account.svg',
   regSuccess: '../../static/images/reg-success.svg',
   user: '../../static/images/pict.jpg',
