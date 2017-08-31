39164a592e35d58129874777c5f6211b6a63dc24
safei muslim <safeydev@gmail.com>
2017-08-31 14:58:42 +0700

    KOM-845 Upload Foto Produk Baru

--- src/Containers/ProductAddStepOne.js
+++ src/Containers/ProductAddStepOne.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Router from 'next/router'
 import FlipMove from 'react-flip-move'
+import winurl from 'winurl'
 // component
 import MyImage from '../Components/MyImage'
 import Wizard from '../Components/Wizard'
 import Notification from '../Components/Notification'
+// actions
+import * as productActions from '../actions/product'
 
 const styles = {
   display: 'flex',
@@ -18,8 +21,13 @@ const styles = {
 class ProductAddStepOne extends Component {
   constructor (props) {
     super(props)
+
+    let { stepOne } = props.tempCreateProduct
+    let images = stepOne.isFound ? stepOne.images : []
+
     this.state = {
-      pictures: [],
+      tempCreateProduct: props.tempCreateProduct || null,
+      images,
       notAcceptedFileType: [],
       notAcceptedFileSize: [],
       notification: {
@@ -27,6 +35,8 @@ class ProductAddStepOne extends Component {
         message: 'Error, default message.'
       }
     }
+
+    this.submiting = false
   }
 
   triggerFileUpload () {
@@ -35,14 +45,16 @@ class ProductAddStepOne extends Component {
 
   onDropFile (e) {
     let files = e.target.files
-    let { pictures, notAcceptedFileType, notAcceptedFileSize, notification } = this.state
+    let { images, notAcceptedFileType, notAcceptedFileSize, notification } = this.state
     notification = {
       status: false,
       message: 'Error, default message.'
     }
+    console.log(files)
     // Iterate over all uploaded files
     for (let i = 0; i < files.length; i++) {
       let f = files[i]
+      console.log(f)
       // Check for file extension
       if (!this.hasExtension(f.name)) {
         notAcceptedFileType.push(f.name)
@@ -63,12 +75,15 @@ class ProductAddStepOne extends Component {
         this.setState({ notAcceptedFileSize, notification })
         continue
       }
-      const reader = new FileReader()
-      reader.onload = (e) => {
-        pictures.push(e.target.result)
-        this.setState({ pictures })
-      }
-      reader.readAsDataURL(f)
+      f['preview'] = winurl.createObjectURL(f)
+      images.push(f)
+      this.setState({ images })
+      // const reader = new FileReader()
+      // reader.onload = (e) => {
+      //   images.push(e.target.result)
+      //   this.setState({ images })
+      // }
+      // reader.readAsDataURL(f)
     }
   }
 
@@ -81,12 +96,32 @@ class ProductAddStepOne extends Component {
   }
 
   removeImage (picture) {
-    const filteredAry = this.state.pictures.filter((e) => e !== picture)
-    this.setState({pictures: filteredAry})
+    const filteredAry = this.state.images.filter((e) => e !== picture)
+    this.setState({images: filteredAry})
+  }
+
+  submit () {
+    const { images, tempCreateProduct } = this.state
+    if (images.length > 0) {
+      this.submiting = true
+      this.props.setTempCreateProduct({ ...tempCreateProduct, stepOne: { isFound: true, images } })
+    } else {
+      this.setState({
+        notification: {
+          status: true,
+          message: 'Foto produk wajib diisi!'
+        }
+      })
+    }
+  }
+
+  componentWillReceiveProps (nextProps) {
+    const { tempCreateProduct } = nextProps
+    if (this.submiting && tempCreateProduct.stepOne.isFound) Router.push('/product-add-step-two')
   }
 
   render () {
-    const { pictures, notification } = this.state
+    const { images, notification } = this.state
     return (
       <section className='section is-paddingless'>
         <Notification
@@ -102,13 +137,13 @@ class ProductAddStepOne extends Component {
             <ul className='add-photo-list'>
               <FlipMove enterAnimation='fade' leaveAnimation='fade' style={styles}>
                 {
-                  pictures.map((picture, index) => {
+                  images.map((picture, index) => {
                     return (
                       <li key={index}>
                         <div className='photo-product'>
                           <a onClick={() => this.removeImage(picture)} className='del-photo'><span className='icon-cross-circle' /></a>
                           <div className='photo-wrapp'>
-                            <MyImage src={picture} alt='' />
+                            <MyImage src={picture.preview} alt='' />
                           </div>
                         </div>
                       </li>
@@ -131,7 +166,7 @@ class ProductAddStepOne extends Component {
             </ul>
             <div className='field'>
               <p className='control'>
-                <button onClick={() => Router.push('/product-add-step-two')} className='button is-primary is-large is-fullwidth'>Lanjutkan</button>
+                <button onClick={() => !this.submiting && this.submit()} className={`button is-primary is-large is-fullwidth ${this.submiting && 'is-loading'}`}>Lanjutkan</button>
               </p>
             </div>
           </div>
@@ -152,11 +187,11 @@ ProductAddStepOne.defaultProps = {
 }
 
 const mapStateToProps = (state) => ({
-
+  tempCreateProduct: state.tempCreateProduct
 })
 
 const mapDispatchToProps = (dispatch) => ({
-
+  setTempCreateProduct: (params) => dispatch(productActions.tempCreateProduct(params))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProductAddStepOne)
