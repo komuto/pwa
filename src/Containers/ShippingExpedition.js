4a0abbde4749c8d3ea9a7e7bc11a4d0215ad32fd
edim <edim.dendy@skyshi.com>
2017-08-31 17:16:56 +0700

    manage with query condition

--- src/Containers/ShippingExpedition.js
+++ src/Containers/ShippingExpedition.js
@@ -1,27 +1,38 @@
 // @flow
 import React from 'react'
 import { connect } from 'react-redux'
+import NProgress from 'nprogress'
 // components
 import Router from 'next/router'
-import Notification from '../Components/Notification'
 // actions
 import * as actionExpeditionTypes from '../actions/expedition'
 import * as actionStoreTypes from '../actions/stores'
-// themes
+// services
+import { Status } from '../Services/Status'
 
 class ShippingExpedition extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
+      manageExpeditions: props.manageExpeditions,
       expeditions: props.expeditions,
       selectedExpeditions: props.processCreateStore.expedition_services.selectedExpeditions,
       selectedServices: props.processCreateStore.expedition_services.selectedServices,
       notification: {
         status: false,
+        color: 'is-success',
         message: 'Error, default message.'
       },
+      setStateStart: false,
       submitting: false
     }
+  }
+
+  handleNotification (e) {
+    const { notification } = this.state
+    const newState = { notification }
+    newState.notification['status'] = !notification.status
+    this.setState(newState)
   }
 
   handleSelectedExpeditions (e, id) {
@@ -61,55 +72,143 @@ class ShippingExpedition extends React.Component {
 
   submitExpedition (e) {
     e.preventDefault()
-    const { selectedServices, selectedExpeditions } = this.state
-    const { postExpedition } = this.props
+    const { expeditions, selectedServices, selectedExpeditions, notification } = this.state
+    const { postExpedition, updateExpedition, query } = this.props
+    const isSetting = this.props.hasOwnProperty('query') && query.type === 'settingStore'
     const tempExpeditionServices = {
       selectedExpeditions: selectedExpeditions,
       selectedServices: selectedServices
     }
-    let { notification } = this.state
     if (selectedServices.length !== 0) {
       this.setState({ submitting: true }, () => {
         if (this.state.submitting) {
-          postExpedition({ expedition_services: tempExpeditionServices })
+          if (isSetting) {
+            const dataServices = []
+            expeditions.expeditions.map(expedition => {
+              return expedition.services.map(service => {
+                return dataServices.push(service.id)
+              })
+            })
+            let newExpedition = []
+            dataServices.map(idService => {
+              let isSelected = selectedServices.filter((Id) => {
+                return Id === idService
+              }).length > 0
+              let statusSelected = isSelected ? 1 : 2
+              newExpedition.push({expedition_service_id: idService, status: statusSelected})
+            })
+            updateExpedition({ data: newExpedition })
+          } else {
+            postExpedition({ expedition_services: tempExpeditionServices })
+          }
         }
       })
     } else {
-      notification = {status: true, message: 'Ekspedisi pengiriman harus di isi !'}
+      const newNotif = { notification }
+      newNotif.notification['status'] = true
+      newNotif.notification['message'] = 'Ekspedisi pengiriman harus di isi !'
+      newNotif.notification['color'] = 'is-danger'
+      this.setState(newNotif)
+    }
+  }
+
+  handleButton () {
+    const { submitting } = this.state
+    const { query } = this.props
+    const isSetting = this.props.hasOwnProperty('query') && query.type === 'settingStore'
+    return (
+      <button
+        className={`button is-primary is-large is-fullwidth ${submitting ? 'is-loading' : ''}`}
+        onClick={(e) => this.submitExpedition(e)} >
+        { isSetting ? 'Simpan Perubahan' : 'Lanjutkan'}
+      </button>
+    )
+  }
+
+  componentDidMount () {
+    const { expeditions, manageExpeditions, selectedServices } = this.state
+    const { getExpedition, query, manageStoreExpeditions } = this.props
+    if (expeditions.expeditions.length === 0) {
+      getExpedition()
+    }
+    if (this.props.hasOwnProperty('query') && query.type === 'settingStore') {
+      if (!manageExpeditions.isFound) {
+        this.setState({ setStateStart: true })
+        manageStoreExpeditions()
+      } else {
+        let serviceId = []
+        manageExpeditions.manageExpeditions.map(exp => {
+          return exp.services.map(service => {
+            serviceId.push(service.id)
+          })
+        })
+        const newService = { selectedServices }
+        newService.selectedServices = serviceId
+        this.setState(newService)
+      }
+    }
+    NProgress.done()
+  }
+
+  componentWillReceiveProps (nextProps) {
+    const { submitting, setStateStart, notification, selectedServices } = this.state
+    const { processCreateStore, expeditions, manageExpeditions, statusUpdateExpedition } = nextProps
+    if (!expeditions.isLoading && expeditions.isFound) {
+      this.setState({ expeditions: expeditions })
+      // selectedExpeditions: processCreateStore.expedition_services.selectedExpeditions, selectedServices: processCreateStore.expedition_services.selectedServices
+    }
+    if (processCreateStore.expedition_services.selectedServices.length !== 0 && submitting) {
+      this.setState({ submitting: false })
+      Router.push('/owner-information')
+    }
+    if (!manageExpeditions.isLoading && manageExpeditions.isFound && setStateStart) {
+      let serviceId = []
+      manageExpeditions.manageExpeditions.map(exp => {
+        return exp.services.map(service => {
+          serviceId.push(service.id)
+        })
+      })
+      const newService = { selectedServices, setStateStart: false }
+      newService.selectedServices = serviceId
+      this.setState(newService)
+    }
+    if (!statusUpdateExpedition.isLoading && statusUpdateExpedition.isFound && submitting) {
+      switch (statusUpdateExpedition.status) {
+        case Status.SUCCESS: {
+          this.props.manageStoreExpeditions()
+          const newNotification = { notification, submitting: false }
+          newNotification.notification['status'] = true
+          newNotification.notification['message'] = statusUpdateExpedition.message
+          newNotification.notification['color'] = 'is-success'
+          this.setState(newNotification)
+          break
+        }
+        case Status.OFFLINE :
+        case Status.FAILED : {
+          const newNotif = { notification, submitting: false }
+          newNotif.notification['status'] = true
+          newNotif.notification['message'] = statusUpdateExpedition.message
+          newNotif.notification['color'] = 'is-danger'
+          this.setState(newNotif)
+          break
+        }
+        default:
+          break
+      }
       this.setState({ notification })
     }
   }
 
-  componentWillMount () {
-    const { expeditions } = this.state
-    const { getExpedition } = this.props
-    if (expeditions.expeditions.length === 0) {
-      getExpedition()
-    }
-  }
-
-  componentWillReceiveProps (nextProps) {
-    this.setState({ expeditions: nextProps.expeditions })
-    this.setState({ selectedExpeditions: nextProps.processCreateStore.expedition_services.selectedExpeditions })
-    this.setState({ selectedServices: nextProps.processCreateStore.expedition_services.selectedServices })
-    if (nextProps.processCreateStore.expedition_services.selectedServices.length !== 0 && this.state.submitting) {
-      this.setState({ submitting: false })
-      Router.push('/owner-information')
-    }
-  }
-
   render () {
-    const { expeditions, selectedServices, selectedExpeditions, notification, submitting } = this.state
+    const { expeditions, selectedServices, selectedExpeditions, notification } = this.state
     return (
       <div>
-        <Notification
-          style={{position: 'fixed'}}
-          type='is-danger'
-          isShow={notification.status}
-          activeClose
-          onClose={() => this.setState({notification: {status: false, message: ''}})}
-          message={notification.message}
-          />
+        <div
+          className={`notification ${notification.status && notification.color}`}
+          style={{display: notification.status ? 'block' : 'none'}}>
+          <button className='delete' onClick={(e) => this.handleNotification(e)} />
+          {notification.message}
+        </div>
         <section className='section is-paddingless has-shadow'>
           <div className='seller-bar'>
             <div className='seller-step active2'>
@@ -171,11 +270,7 @@ class ShippingExpedition extends React.Component {
           <div className='payment-detail action'>
             <ul>
               <li>
-                <a
-                  className={`button is-primary is-large is-fullwidth ${submitting ? 'is-loading' : ''}`}
-                  onClick={(e) => this.submitExpedition(e)}>
-                  Lanjutkan
-                </a>
+                {this.handleButton()}
               </li>
             </ul>
           </div>
@@ -188,13 +283,17 @@ class ShippingExpedition extends React.Component {
 const mapStateToProps = (state) => {
   return {
     processCreateStore: state.tempCreateStore,
-    expeditions: state.expeditions
+    expeditions: state.expeditions,
+    manageExpeditions: state.manageExpeditions,
+    statusUpdateExpedition: state.updateExpedition
   }
 }
 
 const mapDispatchToProps = dispatch => ({
   postExpedition: (params) => dispatch(actionStoreTypes.tempCreateStore(params)),
-  getExpedition: () => dispatch(actionExpeditionTypes.getExpedition())
+  getExpedition: () => dispatch(actionExpeditionTypes.getExpedition()),
+  manageStoreExpeditions: () => dispatch(actionExpeditionTypes.manageStoreExpeditions()),
+  updateExpedition: (params) => dispatch(actionExpeditionTypes.updateExpedition(params))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(ShippingExpedition)
