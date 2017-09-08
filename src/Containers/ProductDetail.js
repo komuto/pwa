c0008d9ecf0e517352db203d61f4576196e21413
edim <edim.dendy@skyshi.com>
2017-09-08 17:34:17 +0700

    add query condition for add product dropship

--- src/Containers/ProductDetail.js
+++ src/Containers/ProductDetail.js
@@ -102,8 +102,15 @@ class ProductDetail extends Component {
     Router.push(`/discussion?id=${this.state.id}`)
   }
 
+  selectProductDropshipper () {
+    const { id } = this.state
+    this.setState({ submiting: !this.state.submiting })
+    Router.push(`/catalog-add-product?id=${id}&commission=${this.props.query.commission}`)
+  }
+
   render () {
     const { productDetail, notification, token, submiting, submitingDiscussion } = this.state
+    const { query } = this.props
     const { detail } = productDetail
     const navbar = {
       searchBoox: false,
@@ -129,7 +136,8 @@ class ProductDetail extends Component {
                 images={detail.images}
                 product={detail.product}
                 rating={detail.rating}
-                notification={(message) => this.notification(message)} />
+                notification={(message) => this.notification(message)}
+                commission={query.commission} />
               <ProductDetailInformation
                 product={detail.product}
                 category={detail.category} />
@@ -140,7 +148,8 @@ class ProductDetail extends Component {
                 product={detail.product}
                 storeData={detail.store} />
               <ProductDetailRule
-                store={detail.store} />
+                store={detail.store}
+                location={detail.location} />
               <ProductDetailSuggestions
                 products={detail.other_products}
                 store={detail.store}
@@ -149,8 +158,10 @@ class ProductDetail extends Component {
                 {...detail.product}
                 purchaseNow={() => this.purchaseNow()}
                 discussion={() => this.discussion()}
+                selectProductDropshipper={() => this.selectProductDropshipper()}
                 submitingDiscussion={submitingDiscussion}
-                submiting={submiting} />
+                submiting={submiting}
+                query={query} />
               </Content>
           }
 
