f4290978d1b96ec4c13826c409a5e35eb6911e01
edim <edim.dendy@skyshi.com>
2017-09-08 17:21:44 +0700

    fix cannot update

--- pages/manage-biodata.js
+++ pages/manage-biodata.js
@@ -1,10 +1,15 @@
 // @flow
+import Router from 'next/router'
 // layout
 import DefaultLayout from '../src/Layout/DefaultLayout'
 // wrapper
 import ReduxPageWrapper from '../src/Utils/ReduxPageWrapper'
 // containers
 import ManageBiodata from '../src/Containers/ManageBiodata'
+
+const toManageAccount = () => {
+  Router.push('/manage-account')
+}
 
 const params = {
   style: 'main user',
@@ -14,13 +19,14 @@ const params = {
   navbar: {
     searchBoox: false,
     path: '/',
+    callBack: () => toManageAccount(),
     textPath: 'Kelola Akun'
   }
 }
 
-const Index = () => (
+const Index = (props) => (
   <DefaultLayout params={params}>
-    <ManageBiodata />
+    <ManageBiodata query={props.query} />
   </DefaultLayout>
 )
 
