91c11f965006bb658488af57ae2042e113f1f68e
safei muslim <safeydev@gmail.com>
2017-09-11 10:38:10 +0700

    set transaction url

--- src/Containers/Tabbar.js
+++ src/Containers/Tabbar.js
@@ -9,8 +9,8 @@ const Tabbar = (props) => {
   return (
     <div className='level nav-bottom is-mobile'>
       <Link href='/'><a className={`level-item has-text-centered ${active === HOME ? 'is-active' : ''}`}><span className='icon-home' />Home</a></Link>
-      <Link href='/transaksi'><a className={`level-item has-text-centered ${active === TRANSAKSI ? 'is-active' : ''}`}><span className='icon-bag' />Transaksi</a></Link>
-      <Link href='/notifikasi'><a className={`level-item has-text-centered ${active === NOTIFIKASI ? 'is-active' : ''}`}><span className='icon-bell' />Notifikasi</a></Link>
+      <Link href='/transaction'><a className={`level-item has-text-centered ${active === TRANSAKSI ? 'is-active' : ''}`}><span className='icon-bag' />Transaksi</a></Link>
+      <Link href='/notification'><a className={`level-item has-text-centered ${active === NOTIFIKASI ? 'is-active' : ''}`}><span className='icon-bell' />Notifikasi</a></Link>
       <Link href='/profile'><a className={`level-item has-text-centered ${active === PROFILE ? 'is-active' : ''}`}><span className='icon-user' />Profil</a></Link>
     </div>
   )
