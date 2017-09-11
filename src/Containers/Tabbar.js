afa7709c27b51dc129d1c759a5b4b561adc94ebf
safei muslim <safeydev@gmail.com>
2017-09-11 16:12:28 +0700

    - change transaksi to transaction - change notifikasi to notification

--- src/Containers/Tabbar.js
+++ src/Containers/Tabbar.js
@@ -2,15 +2,15 @@
 import React from 'react'
 import Link from 'next/link'
 
-import { HOME, TRANSAKSI, NOTIFIKASI, PROFILE } from '../Utils/Constant'
+import { HOME, TRANSACTION, NOTIFICATION, PROFILE } from '../Utils/Constant'
 
 const Tabbar = (props) => {
   const { active } = props.params
   return (
-    <div className='level nav-bottom is-mobile'>
+    <div className='level nav-bottom is-mobile' style={{ zIndex: 1000 }}>
       <Link href='/'><a className={`level-item has-text-centered ${active === HOME ? 'is-active' : ''}`}><span className='icon-home' />Home</a></Link>
-      <Link href='/transaction'><a className={`level-item has-text-centered ${active === TRANSAKSI ? 'is-active' : ''}`}><span className='icon-bag' />Transaksi</a></Link>
-      <Link href='/notification'><a className={`level-item has-text-centered ${active === NOTIFIKASI ? 'is-active' : ''}`}><span className='icon-bell' />Notifikasi</a></Link>
+      <Link href='/transaction'><a className={`level-item has-text-centered ${active === TRANSACTION ? 'is-active' : ''}`}><span className='icon-bag' />Transaksi</a></Link>
+      <Link href='/notification'><a className={`level-item has-text-centered ${active === NOTIFICATION ? 'is-active' : ''}`}><span className='icon-bell' />NOTIFICATION</a></Link>
       <Link href='/profile'><a className={`level-item has-text-centered ${active === PROFILE ? 'is-active' : ''}`}><span className='icon-user' />Profil</a></Link>
     </div>
   )
