976c329ef36d13d8cda55c0c62acb7174d111872
safei muslim <safeydev@gmail.com>
2017-09-18 17:21:17 +0700

    optimize setup params

--- src/Layout/DynamicNavBarLayout.js
+++ src/Layout/DynamicNavBarLayout.js
@@ -5,12 +5,11 @@ import Header from '../Containers/Header'
 // Components
 import Content from '../Components/Content'
 
-export default (props: any) => {
-  const { children } = props
-  const { style, header } = props.params
+export default (props) => {
+  const { children, style, header } = props
   return (
     <Content style={{height: '100%', width: '100%', position: 'absolute'}}>
-      <Header params={header} />
+      <Header {...header} />
       <Content className={`main ${style}`}>
         { children }
       </Content>
