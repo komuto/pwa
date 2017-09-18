b2ba9ee7d5ee0a4869060e7cfc9c3eae9fbc7bd1
safei muslim <safeydev@gmail.com>
2017-09-18 17:23:45 +0700

    - optimize setup params - setup localize

--- pages/categories3.js
+++ pages/categories3.js
@@ -1,22 +1,22 @@
 // @flow
 // layout
-import DynamicNavBarLayout from '../src/Layout/DefaultLayout'
+import DynamicNavBarLayout from '../src/Layout/DynamicNavBarLayout'
 // wrapper
 import ReduxPageWrapper from '../src/Utils/ReduxPageWrapper'
 // containers
 import Categories3 from '../src/Containers/Categories3'
 
-const params = {
-  style: 'categories bg-grey',
-  header: {
-    title: 'Komuto'
-  }
-}
-
 const Index = (props) => {
+  const { localize } = props
+  const params = {
+    style: 'categories bg-grey',
+    header: {
+      title: localize.category
+    }
+  }
   return (
-    <DynamicNavBarLayout params={params}>
-      <Categories3 query={props.query} />
+    <DynamicNavBarLayout {...params} {...props}>
+      <Categories3 {...props} />
     </DynamicNavBarLayout>
   )
 }
