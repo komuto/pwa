ba4eb018035ea69813313c4738c291308a509990
safei muslim <safeydev@gmail.com>
2017-09-18 17:25:03 +0700

    - optimize setup params - setup localize

--- pages/transaction.js
+++ pages/transaction.js
@@ -5,28 +5,30 @@ import DefaultLayout from '../src/Layout/DefaultLayout'
 import ReduxPageWrapper from '../src/Utils/ReduxPageWrapper'
 // containers
 import Transaction from '../src/Containers/Transaction'
-// utils
-import { TRANSACTION } from '../src/Utils/Constant'
-// style custom
-const params = {
-  style: 'main no-padding-bottom bg-grey',
-  header: {
-    title: 'Transaksi'
-  },
-  navbar: {
-    searchBoox: false,
-    textPath: 'Transaksi'
-  },
-  tabbar: {
-    active: TRANSACTION,
-    isShow: true
-  }
-}
+// config
+import Menu from '../src/Config/Menu'
 
-const Index = () => (
-  <DefaultLayout params={params}>
-    <Transaction />
-  </DefaultLayout>
-)
+const Index = (props) => {
+  const { localize } = props
+  const params = {
+    style: 'main no-padding-bottom bg-grey',
+    header: {
+      title: localize.transaction
+    },
+    navbar: {
+      searchBoox: false,
+      textPath: localize.transaction
+    },
+    tabbar: {
+      active: Menu.TRANSACTION,
+      isShow: true
+    }
+  }
+  return (
+    <DefaultLayout {...params} {...props}>
+      <Transaction {...props} />
+    </DefaultLayout>
+  )
+}
 
 export default ReduxPageWrapper(Index)
