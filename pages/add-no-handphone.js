720df0575ed38c39a146ab2c9f1476394c296ef4
safei muslim <safeydev@gmail.com>
2017-09-18 17:22:35 +0700

    optimize setup params

--- pages/add-no-handphone.js
+++ pages/add-no-handphone.js
@@ -6,22 +6,23 @@ import ReduxPageWrapper from '../src/Utils/ReduxPageWrapper'
 // containers
 import AddEditNoHandphone from '../src/Containers/AddEditNoHandphone'
 
-const params = {
-  style: 'main user',
-  header: {
-    title: 'Nomor Handphone'
-  },
-  navbar: {
-    searchBoox: false,
-    path: '/',
-    textPath: 'Nomor Handphone'
+const Index = (props) => {
+  const params = {
+    style: 'main user',
+    header: {
+      title: 'Nomor Handphone'
+    },
+    navbar: {
+      searchBoox: false,
+      path: '/',
+      textPath: 'Nomor Handphone'
+    }
   }
+  return (
+    <DefaultLayout {...params} {...props}>
+      <AddEditNoHandphone {...props} />
+    </DefaultLayout>
+  )
 }
-
-const Index = () => (
-  <DefaultLayout params={params}>
-    <AddEditNoHandphone />
-  </DefaultLayout>
-)
 
 export default ReduxPageWrapper(Index)
