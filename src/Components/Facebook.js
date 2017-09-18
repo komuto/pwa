8c84515dcdecacb171895ecd945699ba68668c44
safei muslim <safeydev@gmail.com>
2017-09-18 17:17:30 +0700

    localize facebook component

--- src/Components/Facebook.js
+++ src/Components/Facebook.js
@@ -5,7 +5,7 @@ export const LoginFacebook = (props) => {
   return <FacebookLogin
     appId='829312977224065'
     fields='name,email,picture'
-    textButton='Login dengan facebook'
+    textButton={props.text}
     callback={(response) => props.responseFacebook(response)}
     cssClass='button is-fb is-large is-fullwidth'
     icon={<span className='icon-fb' />} />
