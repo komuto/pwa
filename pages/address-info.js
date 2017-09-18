4be32041bb2231a9d3235fe4ad6d40eb94ec5447
safei muslim <safeydev@gmail.com>
2017-09-18 17:23:08 +0700

    optimize setup params

--- pages/address-info.js
+++ pages/address-info.js
@@ -6,22 +6,23 @@ import ReduxPageWrapper from '../src/Utils/ReduxPageWrapper'
 // containers
 import AddressInfo from '../src/Containers/AddressInfo'
 
-const params = {
-  style: 'main seller',
-  header: {
-    title: 'Info Alamat'
-  },
-  navbar: {
-    searchBoox: false,
-    path: '/',
-    textPath: 'Info Alamat'
+const Index = (props) => {
+  const params = {
+    style: 'main seller',
+    header: {
+      title: 'Info Alamat'
+    },
+    navbar: {
+      searchBoox: false,
+      path: '/',
+      textPath: 'Info Alamat'
+    }
   }
+  return (
+    <DefaultLayout {...params} {...props}>
+      <AddressInfo {...props} />
+    </DefaultLayout>
+  )
 }
-
-const Index = (props) => (
-  <DefaultLayout params={params}>
-    <AddressInfo query={props.query} />
-  </DefaultLayout>
-)
 
 export default ReduxPageWrapper(Index)
