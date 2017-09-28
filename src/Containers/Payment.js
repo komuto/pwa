dae32279a5ada11a6a95ac8103d9e54ea37fdce4
safei muslim <safeydev@gmail.com>
2017-09-28 15:26:33 +0700

    KOM-1234 Pembayaran

--- src/Containers/Payment.js
+++ src/Containers/Payment.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Router from 'next/router'
-// import NProgress from 'nprogress'
+import NProgress from 'nprogress'
 // Components
 import MyImage from '../Components/MyImage'
 import Loading from '../Components/Loading'
 import Notification from '../Components/Notification'
 import Content from '../Components/Content'
-// Themes
-// import Images from '../Themes/Images'
 // actions
 import * as userActions from '../actions/user'
 import * as paymentActions from '../actions/payment'
 import * as cartActions from '../actions/cart'
 // lib
 import RupiahFormat from '../Lib/RupiahFormat'
-// services
-import { isFetching, isError, isFound, notifError } from '../Services/Status'
 
 class Payment extends Component {
   constructor (props) {
@@ -29,34 +25,28 @@ class Payment extends Component {
       notification: props.notification,
       failTransaction: false
     }
-    this.submiting = {
+    this.submitting = {
       balance: false,
       cart: false,
       snapToken: false,
-      checkoutMidtrans: false,
-      checkoutBalance: false
+      checkout: false
     }
   }
 
-  async componentWillMount () {
-    this.submiting = {
-      ...this.submiting,
+  async componentDidMount () {
+    this.submitting = {
+      ...this.submitting,
       balance: true,
       cart: true
     }
+    NProgress.start()
     await this.props.getBalance()
     await this.props.getCart()
   }
 
   paymentMidtrans () {
-    this.submiting = {...this.submiting, checkoutMidtrans: true}
+    this.submitting = {...this.submitting, checkout: true}
     this.props.setCheckout({'is_wallet': false})
-  }
-
-  paymentBalance () {
-    this.submiting = {...this.submiting, checkoutBalance: true}
-    this.props.setCheckout({'is_wallet': true})
-    // Router.push('/pay-with-balance')
   }
 
   loadMidtransPayment (token) {
@@ -78,47 +68,49 @@ class Payment extends Component {
 
   componentWillReceiveProps (nextProps) {
     let { balance, cart, snapToken, checkout } = nextProps
+    let { isFetching, isError, isFound, notifError } = this.props
 
     // handling state set checkout
-    if (!isFetching(checkout) && this.submiting.checkoutMidtrans) {
+    if (!isFetching(checkout) && this.submitting.checkout) {
       if (isError(checkout)) {
         this.setState({ notification: notifError(checkout.message) })
       }
       if (isFound(checkout)) {
-        this.submiting = {...this.submiting, checkoutMidtrans: false}
+        this.submitting = {...this.submitting, checkout: false}
         this.loadMidtransPayment(snapToken.token)
       }
     }
 
     // handling state get snapToken
-    if (!isFetching(snapToken) && this.submiting.snapToken) {
+    if (!isFetching(snapToken) && this.submitting.snapToken) {
       if (isError(snapToken)) {
         this.setState({ notification: notifError(snapToken.message) })
       }
       if (isFound(snapToken)) {
-        this.submiting = {...this.submiting, snapToken: false}
+        this.submitting = {...this.submitting, snapToken: false}
         this.setState({ snapToken })
       }
     }
 
     // handling state get balance
-    if (!isFetching(balance) && this.submiting.balance) {
+    if (!isFetching(balance) && this.submitting.balance) {
       if (isError(balance)) {
         this.setState({ notification: notifError(balance.message) })
       }
       if (isFound(balance)) {
-        this.submiting = {...this.submiting, balance: false}
+        this.submitting = {...this.submitting, balance: false}
         this.setState({ balance })
       }
     }
 
     // handling state get cart
-    if (!isFetching(cart) && this.submiting.cart) {
+    if (!isFetching(cart) && this.submitting.cart) {
+      NProgress.done()
       if (isError(cart)) {
         this.setState({ notification: notifError(cart.message) })
       }
       if (isFound(cart)) {
-        this.submiting = { ...this.submiting, cart: false, snapToken: true }
+        this.submitting = { ...this.submitting, cart: false, snapToken: true }
         this.props.getMidtransToken({ id: cart.cart.id, platform: 'pwa' })
         this.setState({ cart })
       }
@@ -126,17 +118,7 @@ class Payment extends Component {
   }
 
   render () {
-    let { balance, cart, snapToken, failTransaction, type, notification } = this.state
-    let { promo } = cart.cart
-    let totalPayment = 0
-    if (cart.cart.items) {
-      cart.cart.items.map((item) => {
-        totalPayment += item.total_price
-      })
-    }
-
-    if (promo) totalPayment = totalPayment - promo.nominal
-
+    let { cart, failTransaction, type, notification } = this.state
     if (type && type === 'error') {
       failTransaction = true
     }
@@ -149,56 +131,79 @@ class Payment extends Component {
           activeClose
           onClose={() => this.setState({notification: {status: false, message: ''}})}
           message={notification.message} />
-        <section className='section payment'>
-          <div className='box-rounded'>
-            <div className='total-detail'>
-              <ul className='list-inline col2'>
-                <li>
-                  <span>Total Pembayaran</span>
-                  <span className='price'>Rp { RupiahFormat(totalPayment) }</span>
-                </li>
-                <li className='has-text-right'>
-                  <a onClick={() => Router.push('/shopping-cart')} className='button is-primary is-outlined full-rounded'>Detail</a>
-                </li>
-              </ul>
-              <div className='text-msg'>
-                Harga sudah termasuk pajak dan biaya lainnya
-              </div>
-            </div>
-          </div>
-
-          <div className='title-content'>
-            <h3>Pilih Metode Pembayaran</h3>
-          </div>
-          <div className='box-rounded' onClick={() => this.paymentBalance()}>
-            <div className='payment-method'>
-              Saldo (Rp { RupiahFormat(balance.balance.user_balance) })
-              <span className='icon-arrow-right' />
-            </div>
-          </div>
-          <div className='box-rounded' onClick={() => !this.submiting.snapToken && snapToken.isFound && this.paymentMidtrans()}>
-            <div className='payment-method'>
-              Metode Pembayaran Lainnya
-              {
-                this.submiting.snapToken
-                ? <span className='has-text-right' style={{ position: 'absolute', right: 20 }}><Loading size={14} type='ovals' color='#ef5656' /></span>
-                : <span className='icon-arrow-right' />
-              }
-            </div>
-          </div>
-        </section>
-        <div className='sort-option' style={{ display: failTransaction ? 'block' : 'none' }}>
-          <div className='notif-report'>
-            <MyImage src='../images/reg-success.svg' />
-            <h3>Pembayaran gagal</h3>
-            <p>Mohon maaf kami tidak berhasil melakukan pembayaran anda!</p>
-            <button className='button is-primary is-large is-fullwidth'>Coba lagi</button>
-            <button onClick={() => !this.submiting.snapToken && snapToken.data.isFound && this.paymentMidtrans()} className='button is-primary is-large is-fullwidth is-outlined'>Pilih metode pembayaran lainya</button>
-          </div>
-        </div>
+        { cart.isFound &&
+        <Paymentcontent
+          {...this.state}
+          failTransaction={failTransaction}
+          submitting={this.submitting}
+          paymentMidtrans={() => this.paymentMidtrans()} />
+          }
       </Content>
     )
   }
+}
+
+const Paymentcontent = (props) => {
+  let { cart, snapToken, balance, submitting, failTransaction } = props
+  let totalPayment = 0
+  cart.cart.items.map((item) => {
+    totalPayment += item.total_price
+  })
+
+  return (
+    <Content>
+      <section className='section payment'>
+        <div className='box-rounded'>
+          <div className='total-detail'>
+            <ul className='list-inline col2'>
+              <li>
+                <span>Total Pembayaran</span>
+                <span className='price'>Rp { RupiahFormat(totalPayment) }</span>
+              </li>
+              <li className='has-text-right'>
+                <a onClick={() => Router.push('/shopping-cart')} className='button is-primary is-outlined full-rounded'>Detail</a>
+              </li>
+            </ul>
+            <div className='text-msg'>
+              Harga sudah termasuk pajak dan biaya lainnya
+            </div>
+          </div>
+        </div>
+        <div className='title-content'>
+          <h3>Pilih Metode Pembayaran</h3>
+        </div>
+        <div className='box-rounded' onClick={() => !submitting.balance && balance.isFound && Router.push('/payment-balance')}>
+          <div className='payment-method'>
+            Saldo (Rp { RupiahFormat(balance.balance.user_balance) })
+            {
+              submitting.balance
+              ? <span className='has-text-right' style={{ position: 'absolute', right: 20 }}><Loading size={14} type='ovals' color='#ef5656' /></span>
+              : <span className='icon-arrow-right' />
+            }
+          </div>
+        </div>
+        <div className='box-rounded' onClick={() => !submitting.snapToken && snapToken.isFound && props.paymentMidtrans()}>
+          <div className='payment-method'>
+            Metode Pembayaran Lainnya
+            {
+              submitting.snapToken
+              ? <span className='has-text-right' style={{ position: 'absolute', right: 20 }}><Loading size={14} type='ovals' color='#ef5656' /></span>
+              : <span className='icon-arrow-right' />
+            }
+          </div>
+        </div>
+      </section>
+      <div className='sort-option' style={{ display: failTransaction ? 'block' : 'none' }}>
+        <div className='notif-report'>
+          <MyImage src='../images/reg-success.svg' />
+          <h3>Pembayaran gagal</h3>
+          <p>Mohon maaf kami tidak berhasil melakukan pembayaran anda!</p>
+          <button className='button is-primary is-large is-fullwidth'>Coba lagi</button>
+          <button onClick={() => !this.submitting.snapToken && snapToken.data.isFound && this.paymentMidtrans()} className='button is-primary is-large is-fullwidth is-outlined'>Pilih metode pembayaran lainya</button>
+        </div>
+      </div>
+    </Content>
+  )
 }
 
 const mapStateToProps = (state) => ({
--- src/Containers/PaymentBalance.js
+++ src/Containers/PaymentBalance.js
@@ -1,107 +1,209 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import Router from 'next/router'
 // Component
 import Content from '../Components/Content'
+import Notification from '../Components/Notification'
+// actions
+import * as cartActions from '../actions/cart'
+import * as userActions from '../actions/user'
+// libs
+import RupiahFormat from '../Lib/RupiahFormat'
+import Promo from '../Lib/Promo'
 
 class PaymentBalance extends Component {
-  // constructor (props) {
-  //   super(props)
-  // }
+  constructor (props) {
+    super(props)
+    this.state = {
+      cart: props.cart || null,
+      balance: props.balance || null,
+      checkout: props.checkout || null,
+      notification: props.notification
+    }
+    this.submitting = {
+      balance: false,
+      cart: false,
+      checkout: false
+    }
+  }
+
+  async componentWillMount () {
+    this.submitting = { ...this.submitting, balance: true, cart: true }
+    await this.props.getBalance()
+    await this.props.getCart()
+  }
+
+  paymentBalance () {
+    this.submitting = {...this.submitting, checkout: true}
+    this.props.setCheckout({'is_wallet': true})
+  }
+
+  componentWillReceiveProps (nextProps) {
+    let { isFetching, isError, isFound, notifError } = this.props
+    let { cart, balance, checkout } = nextProps
+
+    // handling state set checkout
+    if (!isFetching(checkout) && this.submitting.checkout) {
+      this.submitting = {...this.submitting, checkout: false}
+      if (isError(checkout)) {
+        this.setState({ notification: notifError(checkout.message) })
+      }
+      if (isFound(checkout)) {
+        Router.push('/payment-success')
+      }
+    }
+
+    // handling state get cart
+    if (!isFetching(cart) && this.submitting.cart) {
+      this.submitting = {...this.submitting, cart: false}
+      if (isError(cart)) {
+        this.setState({ notification: notifError(cart.message) })
+      }
+      if (isFound(cart)) {
+        this.setState({ cart })
+      }
+    }
+    // handling state get balance
+    if (!isFetching(balance) && this.submitting.balance) {
+      this.submitting = {...this.submitting, balance: false}
+      if (isError(balance)) {
+        this.setState({ notification: notifError(balance.message) })
+      }
+      if (isFound(balance)) {
+        this.setState({ balance })
+      }
+    }
+  }
 
   render () {
+    let { cart, balance, notification } = this.state
     return (
       <Content>
-        <section className='section is-paddingless has-shadow'>
-          <div className='payment-detail step-pay'>
-            <ul>
-              <li>
-                <div className='columns is-mobile is-multiline no-margin-bottom'>
-                  <div className='column'>
-                    <div className='label-text is-left'>
-                      <span>Rincian Harga</span>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <ul className='total-pay'>
-                  <li>
-                    <div className='columns is-mobile is-multiline no-margin-bottom'>
-                      <div className='column'>
-                        <div className='label-text is-left'>
-                          <span>
-                            Total Belanja
-                          </span>
-                        </div>
-                      </div>
-                      <div className='column is-one-third'>
-                        <div className='has-text-right'>
-                          <span>Rp 320.000</span>
-                        </div>
-                      </div>
-                    </div>
-                    <div className='columns is-mobile is-multiline no-margin-bottom'>
-                      <div className='column'>
-                        <div className='label-text is-left'>
-                          <span className='pay-code'>
-                            Kode Voucher BELANJAENAK
-                          </span>
-                        </div>
-                      </div>
-                      <div className='column is-one-third'>
-                        <div className='has-text-right'>
-                          <span className='pay-code'> Rp -20.000</span>
-                        </div>
-                      </div>
-                    </div>
-                    <div className='columns is-mobile is-multiline no-margin-bottom'>
-                      <div className='column'>
-                        <div className='label-text is-left'>
-                          <span className='pay-code'>
-                            Saldo
-                          </span>
-                        </div>
-                      </div>
-                      <div className='column is-one-third'>
-                        <div className='has-text-right'>
-                          <span className='pay-code'> Rp -300.000</span>
-                        </div>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div className='columns is-mobile is-multiline no-margin-bottom'>
-                      <div className='column'>
-                        <div className='label-text is-left'>
-                          <span>
-                            Sisa Pembayaran
-                          </span>
-                        </div>
-                      </div>
-                      <div className='column is-one-third'>
-                        <div className='has-text-right'>
-                          <span>Rp 0</span>
-                        </div>
-                      </div>
-                    </div>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </section>
-        <section className='section is-paddingless'>
-          <div className='payment-detail action'>
-            <ul>
-              <li>
-                <span>Dengan menekan tombol "Lanjutkan" Anda telah menyetujui Syarat dan Ketentuan dari Komuto</span>
-                <a className='button is-primary is-large is-fullwidth'>Bayar dengan Saldo</a>
-              </li>
-            </ul>
-          </div>
-        </section>
+        <Notification
+          type={notification.type}
+          isShow={notification.status}
+          activeClose
+          onClose={() => this.setState({notification: {status: false, message: ''}})}
+          message={notification.message} />
+        {
+            cart.isFound &&
+              <PaymentBalanceContent
+                {...cart}
+                {...balance}
+                submitting={this.submitting}
+                paymentBalance={() => this.paymentBalance()} />
+          }
       </Content>
     )
   }
 }
 
-export default PaymentBalance
+const PaymentBalanceContent = (props) => {
+  let { cart, submitting } = props
+  let totalPayment = 0
+  let promoCode = '-'
+  let pricePromo = 0
+
+  cart.items.map((item) => {
+    totalPayment += item.total_price
+  })
+
+  if (cart.promo) {
+    pricePromo = Promo({...cart, totalPayment})
+    promoCode = cart.promo.promo_code
+  }
+
+  return (
+    <Content>
+      <section className='section is-paddingless has-shadow'>
+        <div className='payment-detail step-pay'>
+          <ul>
+            <li>
+              <div className='columns is-mobile is-multiline no-margin-bottom'>
+                <div className='column'>
+                  <div className='label-text is-left'>
+                    <span>Rincian Harga</span>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <ul className='total-pay'>
+                <li>
+                  <div className='columns is-mobile is-multiline no-margin-bottom'>
+                    <div className='column'>
+                      <div className='label-text is-left'>
+                        <span>
+                            Total Belanja
+                          </span>
+                      </div>
+                    </div>
+                    <div className='column is-one-third'>
+                      <div className='has-text-right'>
+                        <span>Rp {RupiahFormat(totalPayment)}</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='columns is-mobile is-multiline no-margin-bottom'>
+                    <div className='column'>
+                      <div className='label-text is-left'>
+                        <span className='pay-code'>
+                            Kode Voucher { promoCode }
+                        </span>
+                      </div>
+                    </div>
+                    <div className='column is-one-third'>
+                      <div className='has-text-right'>
+                        <span className='pay-code'> Rp -{RupiahFormat(pricePromo)}</span>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div className='columns is-mobile is-multiline no-margin-bottom'>
+                    <div className='column'>
+                      <div className='label-text is-left'>
+                        <span>
+                            Sisa Pembayaran
+                          </span>
+                      </div>
+                    </div>
+                    <div className='column is-one-third'>
+                      <div className='has-text-right'>
+                        <span>Rp {RupiahFormat(totalPayment - pricePromo)}</span>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section className='section is-paddingless'>
+        <div className='payment-detail action' style={{ backgroundColor: '#f4f5f6' }}>
+          <ul>
+            <li>
+              <span>Dengan menekan tombol "Lanjutkan" Anda telah menyetujui Syarat dan Ketentuan dari Komuto</span>
+              <a onClick={() => !submitting.checkout && props.paymentBalance()} className={`button is-primary is-large is-fullwidth ${submitting.checkout ? 'is-loading' : ''}`}>Bayar dengan Saldo</a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </Content>
+  )
+}
+
+const mapStateToProps = (state) => ({
+  cart: state.cart,
+  balance: state.balance,
+  checkout: state.checkout
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  getCart: () => dispatch(cartActions.getCart()),
+  getBalance: () => dispatch(userActions.getBalance()),
+  setCheckout: (params) => dispatch(cartActions.checkout(params))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(PaymentBalance)
