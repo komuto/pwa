e3a580def47013e66f79bbf2a9b4891cee739a57
safei muslim <safeydev@gmail.com>
2017-09-29 14:46:59 +0700

    add back router

--- pages/transaction-detail.js
+++ pages/transaction-detail.js
@@ -1,4 +1,4 @@
-// @flow
+import Router from 'next/router'
 // layout
 import DefaultLayout from '../src/Layout/DefaultLayout'
 // wrapper
@@ -15,6 +15,7 @@ const Index = (props) => {
     navbar: {
       searchBoox: false,
       path: '/',
+      callBack: () => Router.push(`/transaction`),
       textPath: 'Detail Transaksi'
     }
   }
