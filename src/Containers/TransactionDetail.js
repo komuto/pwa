74f34bd5c4f1723e3cffa968fc14620cac29fe46
safei muslim <safeydev@gmail.com>
2017-09-29 14:50:40 +0700

    - revamp state from redux - add payment when status transaction is waiting for payment

--- src/Containers/TransactionDetail.js
+++ src/Containers/TransactionDetail.js
@@ -10,10 +10,9 @@ import Notification from '../Components/Notification'
 import * as transactionActions from '../actions/transaction'
 // images
 import Images from '../Themes/Images'
-// services
-import { validateResponse, isFetching } from '../Services/Status'
 // lib
 import RupiahFormat from '../Lib/RupiahFormat'
+import Promo from '../Lib/Promo'
 // payment status
 export const PAYMENT_STATUS = ['add', 'checkout', 'delete', 'Menunggu Pembayaran', 'Verifikasi', 'Kadaluarsa', 'Sudah dibayar', 'Cancel']
 export const PAYMENT_ICON = [Images.paymentWaiting, Images.paymentVerification, Images.paymentExpired, Images.paymentDone]
@@ -37,46 +36,40 @@ class TransactionDetail extends Component {
         message: 'Error, default message.'
       }
     }
+    this.submitting = {
+      transaction: false
+    }
+  }
+
+  showDetail () {
+    this.setState({ detailActive: !this.state.detailActive })
   }
 
   componentDidMount () {
-    const { id } = this.state
+    let { id } = this.state
+    this.submitting = { ...this.submitting, transaction: true }
     NProgress.start()
     this.props.getTransaction({ id })
   }
 
   componentWillReceiveProps (nextProps) {
-    const { transaction } = nextProps
-    if (!isFetching(transaction)) {
+    let { transaction } = nextProps
+    let { isFetching, isError, isFound, notifError } = this.props
+    if (!isFetching(transaction) && this.submitting.transaction) {
       NProgress.done()
-      this.setState({ transaction, notification: validateResponse(transaction, 'Data transaksi tidak ditemukan!') })
+      this.submitting = { ...this.submitting, transaction: false }
+      if (isError(transaction)) {
+        this.setState({ notification: notifError(transaction.message) })
+      }
+
+      if (isFound) {
+        this.setState({ transaction })
+      }
     }
   }
 
   render () {
-    let { transaction, notification, detailActive } = this.state
-    if (!transaction.isFound) return null
-    let { bucket, invoices, summary_transaction } = transaction.transaction
-    let paymentStatus = bucket.status
-    let paymentIcon = PAYMENT_ICON[paymentStatus - 3]
-    let paymentClass = PAYMENT_CLASS[paymentStatus - 3]
-    let price = summary_transaction.total_price
-    let priceAfterPromo = price
-    let discount = 0
-    if (bucket.promo) {
-      let percentage = bucket.promo.percentage / 100
-      let nominal = bucket.promo.nominal
-      if (bucket.promo.type === 0) {
-        discount = ((price * (percentage)))
-        priceAfterPromo = price - discount
-      }
-      if (bucket.promo.type === 1) {
-        discount = nominal
-        priceAfterPromo = price - discount
-      }
-    }
-
-    console.log('transaction', transaction)
+    let { transaction, notification } = this.state
 
     return (
       <Content>
@@ -86,100 +79,134 @@ class TransactionDetail extends Component {
           activeClose
           onClose={() => this.setState({notification: {status: false, message: ''}})}
           message={notification.message} />
-        <section className='section is-paddingless has-shadow'>
-          <div className={`box ${paymentClass}`}>
-            <article className='media'>
-              <div className='media-left'>
-                <figure className='image user-pict' style={paymentIcon === undefined ? { width: 50 } : {}}>
-                  <MyImage src={paymentIcon} />
-                </figure>
-              </div>
-              <div className='media-content'>
-                <div className='content'>
-                  <p>
-                    <strong>{ PAYMENT_STATUS[paymentStatus] }</strong>
+        {
+            transaction.isFound &&
+              <TransactionDetailContent
+                {...this.state}
+                showDetail={() => this.showDetail()} />
+          }
+      </Content>
+    )
+  }
+}
+
+const TransactionDetailContent = (props) => {
+  let { transaction, detailActive, id } = props
+  let { bucket, invoices, summary_transaction } = transaction.transaction
+  let paymentStatus = bucket.status
+  let paymentIcon = PAYMENT_ICON[paymentStatus - 3]
+  let paymentClass = PAYMENT_CLASS[paymentStatus - 3]
+  let { days, hours, minutes } = summary_transaction.time_left
+
+  let totalPayment = summary_transaction.total_price
+  let promoCode = '-'
+  let pricePromo = 0
+
+  if (bucket.promo) {
+    promoCode = bucket.promo.promo_code
+    pricePromo = Promo({ ...bucket, totalPayment })
+  }
+
+  return (
+    <Content>
+      <section className='section is-paddingless has-shadow'>
+        <div className={`box ${paymentClass}`}>
+          <article className='media'>
+            <div className='media-left'>
+              <figure className='image user-pict' style={paymentIcon === undefined ? { width: 50 } : {}}>
+                <MyImage src={paymentIcon} />
+              </figure>
+            </div>
+            <div className='media-content'>
+              <div className='content'>
+                <p>
+                  <strong>{ PAYMENT_STATUS[paymentStatus] }</strong>
+                  <br />
+                  {days} hari  :
+                  {hours} jam  :
+                  {minutes} menit
                   </p>
+              </div>
+            </div>
+          </article>
+        </div>
+        <div className='payment-detail'>
+          <ul>
+            <li>
+              <div className='columns is-mobile is-multiline no-margin-bottom'>
+                <div className='column is-one-third'>
+                  <div className='rating-content is-left'>
+                    <span>Total Tagihan</span>
+                  </div>
+                </div>
+                <div className='column'>
+                  <div className='rating-content is-left has-text-right'>
+                    <span>Rp { RupiahFormat(totalPayment) }</span>
+                    <a onClick={() => props.showDetail()} className='detail-collapsed'>Detail <span className={`icon-arrow-down blue ${detailActive ? 'top' : ''}`} /></a>
+                  </div>
                 </div>
               </div>
-            </article>
-          </div>
-          <div className='payment-detail'>
-            <ul>
-              <li>
-                <div className='columns is-mobile is-multiline no-margin-bottom'>
-                  <div className='column is-one-third'>
-                    <div className='rating-content is-left'>
-                      <span>Total Tagihan</span>
-                    </div>
-                  </div>
-                  <div className='column'>
-                    <div className='rating-content is-left has-text-right'>
-                      <span>Rp { RupiahFormat(summary_transaction.total_price) }</span>
-                      <a onClick={() => this.setState({ detailActive: !this.state.detailActive })} className='detail-collapsed'>Detail <span className={`icon-arrow-down blue ${detailActive ? 'top' : ''}`} /></a>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li className='collapsed' style={{ display: detailActive ? 'block' : 'none' }}>
-                <div className='payment-detail step-pay'>
-                  <ul className='detail-transaction'>
-                    <li>
-                      <ul className='total-pay'>
-                        <li>
-                          <div className='columns is-mobile is-multiline no-margin-bottom'>
-                            <div className='column'>
-                              <div className='label-text is-left'>
-                                <span>
+            </li>
+            <li className='collapsed' style={{ display: detailActive ? 'block' : 'none' }}>
+              <div className='payment-detail step-pay'>
+                <ul className='detail-transaction'>
+                  <li>
+                    <ul className='total-pay'>
+                      <li>
+                        <div className='columns is-mobile is-multiline no-margin-bottom'>
+                          <div className='column'>
+                            <div className='label-text is-left'>
+                              <span>
                                   Total Belanja
                                 </span>
-                              </div>
-                            </div>
-                            <div className='column is-one-third'>
-                              <div className='has-text-right'>
-                                <span>Rp { RupiahFormat(summary_transaction.total_price) }</span>
-                              </div>
                             </div>
                           </div>
-                          <div className='columns is-mobile is-multiline no-margin-bottom'>
-                            <div className='column'>
-                              <div className='label-text is-left'>
-                                <span className='pay-code'>
-                                  Kode Voucher { bucket.promo ? bucket.promo.promo_code : '-' }
-                                </span>
-                              </div>
-                            </div>
-                            <div className='column is-one-third'>
-                              <div className='has-text-right'>
-                                <span className='pay-code'> - Rp { RupiahFormat(discount) }</span>
-                              </div>
+                          <div className='column is-one-third'>
+                            <div className='has-text-right'>
+                              <span>Rp { RupiahFormat(totalPayment) }</span>
                             </div>
                           </div>
-                        </li>
-                        <li>
-                          <div className='columns is-mobile is-multiline no-margin-bottom'>
-                            <div className='column'>
-                              <div className='label-text is-left'>
-                                <span>
+                        </div>
+                        <div className='columns is-mobile is-multiline no-margin-bottom'>
+                          <div className='column'>
+                            <div className='label-text is-left'>
+                              <span className='pay-code'>
+                                  Kode Voucher { promoCode }
+                              </span>
+                            </div>
+                          </div>
+                          <div className='column is-one-third'>
+                            <div className='has-text-right'>
+                              <span className='pay-code'> - Rp { RupiahFormat(pricePromo) }</span>
+                            </div>
+                          </div>
+                        </div>
+                      </li>
+                      <li>
+                        <div className='columns is-mobile is-multiline no-margin-bottom'>
+                          <div className='column'>
+                            <div className='label-text is-left'>
+                              <span>
                                   Sisa Pembayaran
                                 </span>
-                              </div>
-                            </div>
-                            <div className='column is-one-third'>
-                              <div className='has-text-right'>
-                                <span>Rp {RupiahFormat(priceAfterPromo)}</span>
-                              </div>
                             </div>
                           </div>
-                        </li>
-                      </ul>
-                    </li>
-                  </ul>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </section>
-        {
+                          <div className='column is-one-third'>
+                            <div className='has-text-right'>
+                              <span>Rp {RupiahFormat(totalPayment - pricePromo)}</span>
+                            </div>
+                          </div>
+                        </div>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
+      {
           invoices.map((invoice, index) => {
             let { store } = invoice
             return (
@@ -249,9 +276,20 @@ class TransactionDetail extends Component {
             )
           })
         }
-      </Content>
-    )
-  }
+      {
+          paymentStatus === 3 &&
+          <section className='section is-paddingless'>
+            <div className='payment-detail action' style={{ backgroundColor: '#f4f5f6' }}>
+              <ul>
+                <li>
+                  <a onClick={() => Router.push(`/payment?paymentType=transaction&idT=${id}`)} className={`button is-primary is-large is-fullwidth`}>Bayar Sekarang</a>
+                </li>
+              </ul>
+            </div>
+          </section>
+        }
+    </Content>
+  )
 }
 
 const mapStateToProps = (state) => ({
