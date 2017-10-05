f418b94fda76394b71ba16a05dac10b725e35158
safei muslim <safeydev@gmail.com>
2017-10-05 16:29:50 +0700

    new key for midtrans

--- src/Config/AppConfig.js
+++ src/Config/AppConfig.js
@@ -5,10 +5,11 @@ const AppConfig = {
   languages: 'ID', // ID , EN
   midTrans: {
     BASE_URL: 'https://app.sandbox.midtrans.com/snap/snap.js',
+    // BASE_URL: 'https://app.midtrans.com/snnoap/snap.js',
     ACCESS_KEY: {
       MERCHANT_ID: 'M106891',
-      CLIENT_KEY: 'VT-client-2kdvJYszbJGhsBYH',
-      SERVER_KEY: 'VT-server-GbE4FnysSEzYC-jF_kC1x0hZ'
+      CLIENT_KEY: 'VT-client-Y6pK1V-G7F4a1YgU',
+      SERVER_KEY: 'VT-server-eSA2HQ1rSJOqGnIn4ow9wW6j'
     }
   }
 }
