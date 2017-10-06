3e33217abb6a4a467d318e0acbf0d896d11675b4
safei muslim <safeydev@gmail.com>
2017-10-06 17:19:11 +0700

    - create modal slide component - create modal full page component

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -19,6 +19,42 @@ class Modal extends Component {
   }
 }
 
+export const ModalSlide = ({ title, show, showPress, children, style }) => {
+  return (
+    <div className='sort-option sortButton' onClick={(e) => showPress(e)} style={{ display: show ? 'block' : 'none', ...style }}>
+      <div className='sort-list sortButton'>
+        <p><strong>{ title }</strong></p>
+        <form className='form'>
+          <div className='field'>
+            <div className='control popup-option change-address'>
+              { children }
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export const ModalFull = ({ title, show, showPress, children }) => {
+  return (
+    <div className={`modal modal-filter modal-dropship ${show && 'is-active'}`}>
+      <div className='modal-background' />
+      <div className='modal-card'>
+        <header className='modal-card-head'>
+          <p className='modal-card-title'>{title}</p>
+          <button onClick={(e) => showPress(e)} className='delete icon-close' />
+        </header>
+        <section className='modal-card-body'>
+          <div className='edit-data-delivery bg-white'>
+            { children }
+          </div>
+        </section>
+      </div>
+    </div>
+  )
+}
+
 export const Button = (props) => {
   return <button onClick={(e) => props.onClick(e)} className={`button is-primary is-large is-fullwidth ${props.className}`}>{props.text}</button>
 }
