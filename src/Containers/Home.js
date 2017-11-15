a39afee2707aa1a798998d73df82dba1cc212e18
safei muslim <safeydev@gmail.com>
2017-11-15 13:51:55 +0700

    image-categories

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -278,7 +278,7 @@ const CategoryContent = ({ localize, category }) => (
             if (index > 5) return null
             return (
               <div
-                className={`column is-one-third effect-display`}
+                className={`column is-one-third image-categories effect-display`}
                 key={index}
                 onClick={() => {
                   Router.push(
