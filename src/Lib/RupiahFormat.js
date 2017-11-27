8bffb3c35668195f64386d22afd53bfc1c795364
safei muslim <safeydev@gmail.com>
2017-11-27 15:05:46 +0700

    update function rupiah format

--- src/Lib/RupiahFormat.js
+++ src/Lib/RupiahFormat.js
@@ -1,2 +1,12 @@
-const RupiahFormat = (x) => String(x).replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+const RupiahFormat = (price, fixed = 2) => {
+  const priceSplit = String(price.toFixed(fixed)).split('.')
+  const firstPrice = priceSplit[0]
+  const secondPrice = priceSplit[1]
+  const priceReal = String(firstPrice).replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+  if (Number(secondPrice) > 0) {
+    return `${priceReal},${secondPrice}`
+  } else {
+    return `${priceReal}`
+  }
+}
 export default RupiahFormat
