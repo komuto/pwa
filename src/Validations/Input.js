2957a21b7bcd6cdb52c7aa5d6e030394e45bcae1
edim dendy <edim.dendy@skyshi.com>
2017-11-28 15:24:16 +0700

    fix image products

--- src/Validations/Input.js
+++ src/Validations/Input.js
@@ -11,8 +11,22 @@ const inputNumber = (input) => {
   return input.replace(/[^0-9]/g, '')
 }
 
+/**
+  * prefix handphone list
+  +62811/, /+62812/, /+62813/, /+62851/, /+62852/, /+62853/, /+62823/, /0811/, /0812/, /0813/, /0851/, /0852/, /0853/, /0823/, // telkomsel
+  /+62856/, /+62855/, /+62857/, /+62815/, /+62816/, /+62858/, /0856/, /0855/, /0857/, /0815/, /0816/, /0858/, // indosat
+  /+62859/, /+62817/, /+62818/, /+62819/, /+62878/, /+62877/, /0859/, /0817/, /0818/, /0819/, /0878/, /0877/, // XL
+  /+6283/, /083/, // axis
+  /+6289/, /098/ // three
+*/
+const inputPhoneNumber = (input) => {
+  const prefixPhoneList = /(\+62|62|0)(811|812|813|851|852|853|823|856|855|857|815|816|858|859|817|818|819|878|877|83|89|98)/g
+  return prefixPhoneList.test(input)
+}
+
 export {
   inputNormal,
   inputString,
-  inputNumber
+  inputNumber,
+  inputPhoneNumber
 }
