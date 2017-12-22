33a5d8b5ccd6f7be3e0a7c81c88afeaa09d4848c
safei muslim <safeydev@gmail.com>
2017-12-22 10:32:40 +0700

    alert tab notification

--- src/Containers/Tabbar.js
+++ src/Containers/Tabbar.js
@@ -10,7 +10,7 @@ const Tabbar = (props) => {
     <div className='level nav-bottom is-mobile' style={{ zIndex: 1000 }}>
       <Link href='/'><a className={`level-item has-text-centered ${tabbar.active === Menu.HOME ? 'is-active' : ''}`}><span className='icon-home' />{localize.home}</a></Link>
       <a onClick={() => props.isLogin ? Router.push('/transaction') : props.alertLogin()} className={`level-item has-text-centered ${tabbar.active === Menu.TRANSACTION ? 'is-active' : ''}`}><span className='icon-bag' />{localize.transaction}</a>
-      <Link href='/notification'><a className={`level-item has-text-centered ${tabbar.active === Menu.NOTIFICATION ? 'is-active' : ''}`}><span className='icon-bell' />{localize.notification}</a></Link>
+      <a onClick={() => props.isLogin ? Router.push('/notification') : props.alertLogin()} className={`level-item has-text-centered ${tabbar.active === Menu.NOTIFICATION ? 'is-active' : ''}`}><span className='icon-bell' />{localize.notification}</a>
       <Link href='/profile'><a className={`level-item has-text-centered ${tabbar.active === Menu.PROFILE ? 'is-active' : ''}`}><span className='icon-user' />{localize.profile}</a></Link>
     </div>
   )
